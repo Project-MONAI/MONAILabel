d25f2ec987d23e871f2be58011facbd937fea58f
SACHIDANAND ALLE <sachidanand.alle@gmail.com>
2021-05-07 10:11:45 -0700

    Add train/validation status (#60)
    Signed-off-by: Sachidanand Alle <salle@nvidia.com>
    
    Co-authored-by: Sachidanand Alle <salle@nvidia.com>

--- monailabel/utils/train/task.py
+++ monailabel/utils/train/task.py
@@ -3,6 +3,8 @@ import logging
 import time
 from abc import abstractmethod
 
+import torch
+
 from monai.engines import SupervisedTrainer, SupervisedEvaluator
 
 logger = logging.getLogger(__name__)
@@ -139,7 +141,7 @@ class TrainTask:
         return None
 
     def evaluator(self):
-        return SupervisedEvaluator(
+        self._evalutor = SupervisedEvaluator(
             device=self.device(),
             val_data_loader=self.val_data_loader(),
             network=self.network().to(self.device()),
@@ -149,6 +151,7 @@ class TrainTask:
             additional_metrics=self.val_additional_metrics(),
             val_handlers=self.val_handlers()
         ) if self.val_data_loader() else None
+        return self._evalutor
 
     def __call__(self, max_epochs, amp):
         trainer = SupervisedTrainer(
@@ -171,7 +174,28 @@ class TrainTask:
         lapsed = str(datetime.timedelta(seconds=int(time.time() - start)))
         logger.info(f"++ Total Train Time:: {lapsed}")
 
+        def tensor_to_list(d):
+            for k, v in d.items():
+                d[k] = v.tolist() if torch.is_tensor(v) else v
+
         stats = trainer.get_train_stats()
         stats['total_time'] = lapsed
-        logger.info(f"Train Stats: {stats}")
+        stats['train_metrics'] = trainer.state.metrics
+        stats['train_metric_details'] = trainer.state.metric_details
+        stats['train_key_metric_name'] = trainer.state.key_metric_name
+        stats['train_best_metric'] = trainer.state.best_metric_epoch
+        stats['train_best_metric_epoch'] = trainer.state.best_metric_epoch
+        tensor_to_list(stats['train_metrics'])
+        tensor_to_list(stats['train_metric_details'])
+
+        if self._evalutor:
+            stats['val_metrics'] = self._evalutor.state.metrics
+            stats['val_metric_details'] = self._evalutor.state.metric_details
+            stats['val_key_metric_name'] = self._evalutor.state.key_metric_name
+            stats['val_best_metric'] = self._evalutor.state.best_metric_epoch
+            stats['val_best_metric_epoch'] = self._evalutor.state.best_metric_epoch
+            tensor_to_list(stats['val_metrics'])
+            tensor_to_list(stats['val_metric_details'])
+
+        logger.info(f"Train Task Stats: {stats}")
         return stats
