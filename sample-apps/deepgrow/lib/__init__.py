b157ffa343e231c76f4fb96f8a1be028d1ce5387
SACHIDANAND ALLE <sachidanand.alle@gmail.com>
2021-05-18 19:04:19 +0100

    fix deepgrow to support pipeline for inference (#74)
    Signed-off-by: Sachidanand Alle <salle@nvidia.com>
    
    Co-authored-by: Sachidanand Alle <salle@nvidia.com>

--- sample-apps/deepgrow/lib/__init__.py
+++ sample-apps/deepgrow/lib/__init__.py
@@ -1,4 +1,4 @@
 from .activelearning import MyStrategy
 from .infer import InferDeepgrow, InferSpleen
-from .train import TrainDeepgrow, TrainSpleen
+from .train import TrainDeepgrow
 from .transforms import Random2DSlice
