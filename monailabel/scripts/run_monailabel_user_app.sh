82d7260aa0361fe82beb62c42f0ed587c8d6c8f4
root <root@ipp1-0322.nvidia.com>
2021-05-21 19:39:16 -0700

    Remove user requirements and inherit system packages for venv
    Signed-off-by: root <root@ipp1-0322.nvidia.com>

--- monailabel/scripts/run_monailabel_user_app.sh
+++ monailabel/scripts/run_monailabel_user_app.sh
@@ -16,29 +16,24 @@ using_vnv=false
 
 if [[ -f requirements.txt ]]; then
   if [[ ! -d .venv ]]; then
-    python3 -m venv .venv
+    python -m venv --system-site-packages .venv
   fi
 
   # try to activate an existing environment, or re-create an try to activate
   # if unable to activate the existing environment
-  source .venv/bin/activate || (python3 -m venv .venv && source .venv/bin/activate)
+  source .venv/bin/activate || (python -m venv --system-site-packages .venv && source .venv/bin/activate)
   using_vnv=true
 
   # always ensure the user packages are up to date
   python -m pip install --upgrade pip >/dev/null
 
-  tmpfile=$(mktemp /tmp/requirements_XXXXXX.txt)
-  cat "$(dirname "$(readlink -f "$0")")/user-requirements.txt" <(echo) requirements.txt >>${tmpfile}
-
   echo "+++++++++++++++++ Installing PIP requirements"
-  python -m pip install -r ${tmpfile} >/dev/null
+  python -m pip install -r requirements.txt
   if [[ $? -ne 0 ]]; then
     echo "Failed to initialize APP venv"
-    rm ${tmpfile}
     deactivate
     exit 1
   fi
-  rm ${tmpfile}
 fi
 
 popd >/dev/null
