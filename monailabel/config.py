e7208ac83d0754def09b33f0842416a9aa96ccc4
SACHIDANAND ALLE <sachidanand.alle@gmail.com>
2021-05-25 11:04:23 +0100

    Add run-time settings through env while running app (#89)
    * Support settings for app
    
    Signed-off-by: SACHIDANAND ALLE <sachidanand.alle@gmail.com>
    
    * Fix ci checks
    
    Signed-off-by: Sachidanand Alle <salle@nvidia.com>
    
    Co-authored-by: Sachidanand Alle <salle@nvidia.com>

--- monailabel/config.py
+++ monailabel/config.py
@@ -1,25 +1,24 @@
-from typing import List, Union
+from typing import List
 
-from pydantic import AnyHttpUrl, BaseSettings, validator
+from pydantic import AnyHttpUrl, BaseSettings
 
 
 class Settings(BaseSettings):
     API_STR: str = ""
-    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
     PROJECT_NAME: str = "MONAILabel"
 
     APP_DIR: str = ""
     STUDIES: str = ""
+    APP_PORT: int = 8000
 
-    @validator("BACKEND_CORS_ORIGINS", pre=True)
-    def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
-        if isinstance(v, str) and not v.startswith("["):
-            return [i.strip() for i in v.split(",")]
-        elif isinstance(v, (list, str)):
-            return v
-        raise ValueError(v)
+    DATASTORE_AUTO_RELOAD: bool = True
+    DATASTORE_IMAGE_EXT: List[str] = ["*.nii.gz", "*.nii"]
+    DATASTORE_LABEL_EXT: List[str] = ["*.nii.gz", "*.nii"]
+
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     class Config:
+        env_file = ".env"
         case_sensitive = True
 
 
