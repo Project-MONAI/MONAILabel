39d98f651dcfc3234c16012b76c962b40fadff5b
Andres Diaz-Pinto <diazandr3s@gmail.com>
2021-06-07 02:46:57 +0100

    Update test file

--- sample-apps/deepedit_brain-tumour/test.py
+++ sample-apps/deepedit_brain-tumour/test.py
@@ -7,9 +7,9 @@ if __name__ == "__main__":
 # Example commands to run inference or train locally
 
 # Inference
-python test.py -a . -s MSD_Task02_Heart/imagesTr infer -m segmentation -i la_004.nii.gz -o label.nii.gz
+python test.py -a . -s MSD_Task01_BrainTumour_single/imagesTr infer -m segmentation -i BRATS_001.nii.gz -o label_final_BRATS_001.nii.gz
 
 # Train
-python test.py -a . -s MSD_Task02_Heart/imagesTr train -n model_01
+python test.py -a . -s MSD_Task01_BrainTumour_single/imagesTr train -n model_01
 
 """
