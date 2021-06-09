8a678b363110185856bbefa30a5fe32a99804a24
Sachidanand Alle <salle@nvidia.com>
2021-06-08 17:52:15 -0700

    enable package and dosc build
    Signed-off-by: Sachidanand Alle <salle@nvidia.com>

--- monailabel/__init__.py
+++ monailabel/__init__.py
@@ -1,4 +1,26 @@
+import os
 import sys
+
+from ._version import get_versions
+
+PY_REQUIRED_MAJOR = 3
+PY_REQUIRED_MINOR = 6
+
+version_dict = get_versions()
+__version__ = version_dict.get("version", "0+unknown")
+__revision_id__ = version_dict.get("full-revisionid")
+del get_versions, version_dict
+
+__copyright__ = "(c) 2020 - 2021 MONAI Consortium"
+
+__basedir__ = os.path.dirname(__file__)
+
+if not (sys.version_info.major == PY_REQUIRED_MAJOR and sys.version_info.minor >= PY_REQUIRED_MINOR):
+    raise RuntimeError(
+        "MONAILabel requires Python {}.{} or higher. But the current Python is: {}".format(
+            PY_REQUIRED_MAJOR, PY_REQUIRED_MINOR, sys.version
+        ),
+    )
 
 
 def print_config(file=sys.stdout):
@@ -8,10 +30,10 @@ def print_config(file=sys.stdout):
     import torch
 
     output = OrderedDict()
-    output["MONAILabel"] = "0.1"
+    output["MONAILabel"] = __version__
     output["Numpy"] = np.version.full_version
     output["Pytorch"] = torch.__version__
 
     for k, v in output.items():
         print(f"{k} version: {v}", file=file, flush=True)
-    print("MONAILabel rev id: 0.1")
+    print(f"MONAILabel rev id: {__revision_id__}")
--- setup.py
+++ setup.py
@@ -3,32 +3,18 @@ from setuptools import find_packages, setup
 
 import versioneer
 
-with open("README.md") as f:
-    readme = f.read()
-
-with open("LICENSE") as f:
-    license_desc = f.read()
-
-with open("requirements.txt") as f:
-    requirements = [l.strip() for l in f]
-
 setup(
-    name="monailabel",
     version=versioneer.get_version(),
-    description="MONAI Label Development Kit",
-    long_description=readme,
-    url="http://monai.io",
-    license=license_desc,
-    packages=find_packages(exclude=("tests", "docs", "apps")),
-    python_requires=">=3.6",
-    install_requires=requirements,
+    packages=find_packages(exclude=("tests", "docs", "sample-apps", "plugins")),
+    zip_safe=False,
+    package_data={"monailabel": ["py.typed"]},
+    include_package_data=True,
     scripts=[
         "monailabel/monailabel",
         "monailabel/monailabel.bat",
         "monailabel/scripts/run_monailabel_app.sh",
         "monailabel/scripts/run_monailabel_app.bat",
     ],
-    include_package_data=True,
     data_files=[
         ("logconfig", ["monailabel/logging.json"]),
     ],
