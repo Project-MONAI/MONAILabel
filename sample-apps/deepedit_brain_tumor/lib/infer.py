d875d6b919527eb48f9c47a157a92f9a86614cad
Andres Diaz-Pinto <diazandr3s@gmail.com>
2021-06-29 12:21:46 +0200

    Add transform for infer (#149)
    * Update transforms for infer
    
    Signed-off-by: Andres Diaz-Pinto <diazandr3s@gmail.com>
    
    * Autofix
    
    Signed-off-by: Andres Diaz-Pinto <diazandr3s@gmail.com>

--- sample-apps/deepedit_brain_tumor/lib/infer.py
+++ sample-apps/deepedit_brain_tumor/lib/infer.py
@@ -15,7 +15,7 @@ from monai.transforms import (
     ToTensord,
 )
 
-from monailabel.deepedit.transforms import DiscardAddGuidanced
+from monailabel.deepedit.transforms import DiscardAddGuidanced, ResizeGuidanceCustomd
 from monailabel.interfaces.tasks import InferTask, InferType
 from monailabel.utils.others.post import Restored
 
