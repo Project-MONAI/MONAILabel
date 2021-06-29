5f82af62026be218bc439406391b7bfb38436233
Alvin Ihsani <aihsani@nvidia.com>
2021-06-29 15:42:44 -0400

    test autosegmentation inference workflow
    Signed-off-by: Alvin Ihsani <aihsani@nvidia.com>

---
+++ tests/integration/test_infer.py
@@ -0,0 +1,19 @@
+import unittest
+
+import requests
+
+from . import SERVER_URI
+
+
+class EndPointInfer(unittest.TestCase):
+
+    def test_segmentation(self):
+        model = "segmentation_left_atrium"
+        image = "la_004.nii.gz"
+
+        response = requests.post(f"{SERVER_URI}/infer/{model}?image={image}")
+        assert response.status_code == 200
+
+
+if __name__ == "__main__":
+    unittest.main()
