d5944de91c0b0e6ab087a6924e03364ea405509d
Alvin Ihsani <aihsani@nvidia.com>
2021-07-02 09:17:25 -0400

    fixing docs conf import order
    Signed-off-by: Alvin Ihsani <aihsani@nvidia.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,17 +6,17 @@
 
 # -- Path setup --------------------------------------------------------------
 
-import monailabel
 import os
 import sys
+sys.path.insert(0, os.path.abspath("../../"))
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import sphinx_rtd_theme
+import monailabel
 
-sys.path.insert(0, os.path.abspath("../../"))
 
 # -- Project information -----------------------------------------------------
 
