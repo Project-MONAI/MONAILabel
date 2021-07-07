d05d5ce9159d7ceafc60ab8401645ab85d2b0b84
Andres Diaz-Pinto <diazandr3s@gmail.com>
2021-07-07 14:56:04 +0200

    Update DeepEdit - MONAI 0.6 (#175)
    * Update DeepEdit - MONAI 0.6
    
    Signed-off-by: Andres <diazandr3s@gmail.com>
    
    * Update YAML files
    
    Signed-off-by: Andres <diazandr3s@gmail.com>
    
    Co-authored-by: Sachidanand Alle <salle@nvidia.com>

--- sample-apps/deepedit_lung/main.py
+++ sample-apps/deepedit_lung/main.py
@@ -2,7 +2,7 @@ import logging
 import os
 
 from lib import Deepgrow, MyStrategy, MyTrain, Segmentation
-from monai.networks.nets import DynUNet
+from monai.networks.nets.dynunet_v1 import DynUNetV1
 
 from monailabel.interfaces import MONAILabelApp
 from monailabel.utils.activelearning import Random
@@ -12,7 +12,7 @@ logger = logging.getLogger(__name__)
 
 class MyApp(MONAILabelApp):
     def __init__(self, app_dir, studies):
-        self.network = DynUNet(
+        self.network = DynUNetV1(
             spatial_dims=3,
             in_channels=3,
             out_channels=1,
