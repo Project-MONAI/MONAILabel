fbc17759b8905f3f8aa75bb9ccfca549e54aa4db
Sachidanand Alle <sachidanand.alle@gmail.com>
2021-08-02 14:22:28 -0700

    Fix build
    Signed-off-by: Sachidanand Alle <sachidanand.alle@gmail.com>

--- setup.py
+++ setup.py
@@ -23,9 +23,9 @@ data_files.extend(recursive_files("sample-apps", "monailabel"))
 data_files.extend(recursive_files("plugins/slicer", "monailabel"))
 
 # Build OHIF Plugin
-build_ohif = os.environ.get("BUILD_OHIF")
-print(f"BUILD_OHIF = {build_ohif}")
-build_ohif = True if not build_ohif else distutils.util.strtobool(build_ohif)
+build_ohif_s = os.environ.get("BUILD_OHIF")
+print(f"BUILD_OHIF = {build_ohif_s}")
+build_ohif = True if not build_ohif_s else distutils.util.strtobool(build_ohif_s)
 if build_ohif:
     script = "build.bat" if any(platform.win32_ver()) else "build.sh"
     command = os.path.realpath(os.path.join(os.path.dirname(__file__), "plugins", "ohif", script))
