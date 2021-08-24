06d2768411b6ea729f9e9ff4b222147bf188c769
Andres <diazandr3s@gmail.com>
2021-08-24 03:41:19 +0100

    Fix deepedit transform -- Add licenses
    Signed-off-by: Andres <diazandr3s@gmail.com>

--- sample-apps/deepedit_spleen/lib/train.py
+++ sample-apps/deepedit_spleen/lib/train.py
@@ -1,3 +1,14 @@
+# Copyright 2020 - 2021 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 
 import torch
@@ -69,7 +80,7 @@ class MyTrain(BasicTrainTask):
             FindDiscrepancyRegionsd(label="label", pred="pred", discrepancy="discrepancy"),
             ClickRatioAddRandomGuidanced(guidance="guidance", discrepancy="discrepancy", probability="probability"),
             AddGuidanceSignald(image="image", guidance="guidance"),
-            DiscardAddGuidanced(image="image", probability=0.5),
+            DiscardAddGuidanced(keys="image", probability=0.5),
             ToTensord(keys=("image", "label")),
         ]
 
@@ -95,7 +106,7 @@ class MyTrain(BasicTrainTask):
             FindAllValidSlicesd(label="label", sids="sids"),
             AddInitialSeedPointd(label="label", guidance="guidance", sids="sids"),
             AddGuidanceSignald(image="image", guidance="guidance"),
-            DiscardAddGuidanced(image="image", probability=0.5),
+            DiscardAddGuidanced(keys="image", probability=0.5),
             ToTensord(keys=("image", "label")),
         ]
 
