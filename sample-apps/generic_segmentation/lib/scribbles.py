f6418255875660693b160818cd81649c4c80dc12
masadcv <muhammad.asad@kcl.ac.uk>
2021-09-06 10:16:59 +0100

    Add ROI select method to define a boundingbox for coldstart  scribbles methods (#325)
    * moving reusable scribbles code to ./monailabel/scribbles/.
    
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>
    
    * change additional imports to optional imports for scribbles tx utils
    
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>
    
    * change additional imports to optional imports for scribbles tx utils 2
    
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>
    
    * adding scribbles to generic segmentation app
    
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>
    
    * adding roi based bbox support to mark bg scribbles
    
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>
    
    * removing commented out scribbles methods
    
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>
    
    * fix codeformat issues
    
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>
    
    * update bins argument, selected_roi cond statement
    
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>
    
    Co-authored-by: SACHIDANAND ALLE <sachidanand.alle@gmail.com>

--- sample-apps/generic_segmentation/lib/scribbles.py
+++ sample-apps/generic_segmentation/lib/scribbles.py
@@ -13,6 +13,7 @@ from monai.transforms import Compose, EnsureChannelFirstd, LoadImaged, Orientati
 
 from monailabel.interfaces.tasks import InferTask, InferType
 from monailabel.scribbles.transforms import (
+    AddBackgroundScribblesFromROId,
     ApplyCRFOptimisationd,
     ApplyGraphCutOptimisationd,
     ApplyISegGraphCutPostProcd,
@@ -143,6 +144,7 @@ class GenericISegGraphcutColdstart(ScribblesInferTask):
         return [
             LoadImaged(keys=["image", "label"]),
             EnsureChannelFirstd(keys=["image", "label"]),
+            AddBackgroundScribblesFromROId(scribbles="label", scribbles_bg_label=2, scribbles_fg_label=3),
             # at the moment optimisers are bottleneck taking a long time,
             # therefore scaling non-isotropic with big spacing
             Spacingd(keys=["image"], pixdim=[2.5, 2.5, 5.0], mode="bilinear"),
