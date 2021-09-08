227975b3a180f04dfe7f9958241f8f1aec55c06a
Sachidanand Alle <sachidanand.alle@gmail.com>
2021-09-07 20:28:56 -0700

    Enable test case
    Signed-off-by: Sachidanand Alle <sachidanand.alle@gmail.com>

--- tests/unit/endpoints/test_train.py
+++ tests/unit/endpoints/test_train.py
@@ -8,6 +8,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import time
 import unittest
 
@@ -17,7 +18,7 @@ from .context import BasicEndpointTestSuite
 
 
 class TestEndPointTrain(BasicEndpointTestSuite):
-    def xtest_001_train(self):
+    def test_001_train(self):
         if not torch.cuda.is_available():
             return
 
