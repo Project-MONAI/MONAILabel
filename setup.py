e51b3ea1024fb6c94c8930e6d9991e419e9eec6f
Alvin Ihsani <aihsani@nvidia.com>
2021-09-22 09:34:07 -0400

    removing imports
    Signed-off-by: Alvin Ihsani <aihsani@nvidia.com>

--- setup.py
+++ setup.py
@@ -11,10 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import distutils
 import os
-import platform
-import subprocess
 
 from setuptools import find_packages, setup
 
