8c916b81baee04bb7b6f20b76eefeaa9cd71de1f
SACHIDANAND ALLE <sachidanand.alle@gmail.com>
2021-10-19 23:08:37 +0100

    fix utilities + logs (#465)
    Signed-off-by: Sachidanand Alle <sachidanand.alle@gmail.com>

--- monailabel/interfaces/tasks/infer.py
+++ monailabel/interfaces/tasks/infer.py
@@ -260,7 +260,7 @@ class InferTask:
         return result_file_name, result_json
 
     def run_pre_transforms(self, data, transforms):
-        return run_transforms(data, transforms, log_prefix="PRE")
+        return run_transforms(data, transforms, log_prefix="PRE", use_compose=True)
 
     def run_invert_transforms(self, data, pre_transforms, names):
         if names is None:
