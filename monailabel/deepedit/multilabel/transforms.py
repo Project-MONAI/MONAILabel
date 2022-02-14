ec4bbcb967717a92b58e3c3dfcde388ea9410b66
Andres <diazandr3s@gmail.com>
2022-02-14 21:32:48 +0000

    Upgrade multilabel DeepEdit to work for single label
    Signed-off-by: Andres <diazandr3s@gmail.com>

--- monailabel/deepedit/multilabel/transforms.py
+++ monailabel/deepedit/multilabel/transforms.py
@@ -610,7 +610,6 @@ class AddRandomGuidanceCustomd(Randomizable, MapTransform):
 
             # Checking the number of clicks
             num_clicks = random.randint(1, 10)
-            logger.info(f"Number of simulated clicks: {num_clicks}")
             counter = 0
             keep_guidance = []
             while True:
@@ -625,12 +624,13 @@ class AddRandomGuidanceCustomd(Randomizable, MapTransform):
                         for key_label in d["label_names"].keys():
                             if key_label not in keep_guidance:
                                 self.tmp_guidance[key_label] = []
+                        logger.info(f"Number of simulated clicks: {counter}")
                         break
 
-            # Convert tmp_guidance back to json
-            for key_label in d["label_names"].keys():
-                d[self.guidance][key_label] = json.dumps(np.asarray(self.tmp_guidance[key_label]).astype(int).tolist())
-            #
+                # Breaking once all labels are covered
+                if len(keep_guidance) == len(d["label_names"].keys()):
+                    logger.info(f"Number of simulated clicks: {counter}")
+                    break
 
         return d
 
