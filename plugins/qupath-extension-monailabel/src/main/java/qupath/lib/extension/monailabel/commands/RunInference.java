467099b0ad1ef41cb34ac5e10be1761b2e76566a
SACHIDANAND ALLE <sachidanand.alle@gmail.com>
2022-04-25 20:31:05 +0100

    pre_commit checks
    Signed-off-by: SACHIDANAND ALLE <sachidanand.alle@gmail.com>

--- plugins/qupath-extension-monailabel/src/main/java/qupath/lib/extension/monailabel/commands/RunInference.java
+++ plugins/qupath-extension-monailabel/src/main/java/qupath/lib/extension/monailabel/commands/RunInference.java
@@ -1,3 +1,16 @@
+/*
+Copyright (c) MONAI Consortium
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package qupath.lib.extension.monailabel.commands;
 
 import java.awt.Color;
@@ -37,12 +50,6 @@ import qupath.lib.roi.RectangleROI;
 import qupath.lib.roi.interfaces.ROI;
 import qupath.lib.scripting.QP;
 
-/**
- * Run Inference.
- * 
- * @author Sachidanand Alle
- *
- */
 public class RunInference implements Runnable {
 	private final static Logger logger = LoggerFactory.getLogger(RunInference.class);
 
