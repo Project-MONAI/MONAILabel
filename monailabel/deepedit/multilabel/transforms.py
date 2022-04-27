725756bbec1a8944060c9ce3a42bed356f1673b7
Andres Diaz-Pinto <diazandr3s@gmail.com>
2022-04-27 11:23:35 +0100

    Recover deleted transforms
    Signed-off-by: Andres Diaz-Pinto <diazandr3s@gmail.com>

--- monailabel/deepedit/multilabel/transforms.py
+++ monailabel/deepedit/multilabel/transforms.py
@@ -253,6 +253,185 @@ class AddGuidanceSignalCustomd(MapTransform):
         return d
 
 
+class FindAllValidSlicesCustomd(MapTransform):
+    """
+    Find/List all valid slices in the labels.
+    Label is assumed to be a 4D Volume with shape CHWD, where C=1.
+    Args:
+        label: key to the label source.
+        sids: key to store slices indices having valid label map.
+    """
+
+    def __init__(
+        self,
+        keys: KeysCollection,
+        sids="sids",
+        allow_missing_keys: bool = False,
+    ):
+        super().__init__(keys, allow_missing_keys)
+        self.sids = sids
+
+    def _apply(self, label, d):
+        sids = {}
+        for key_label in d["label_names"].keys():
+            l_ids = []
+            for sid in range(label.shape[-1]):  # Assume channel is first and depth is last CHWD
+                if d["label_names"][key_label] in label[0][..., sid]:
+                    l_ids.append(sid)
+            sids[key_label] = l_ids
+        return sids
+
+    def __call__(self, data: Mapping[Hashable, np.ndarray]) -> Dict[Hashable, np.ndarray]:
+        d: Dict = dict(data)
+        for key in self.key_iterator(d):
+            if key == "label":
+                label = d[key]
+                if label.shape[0] != 1:
+                    raise ValueError("Only supports single channel labels!")
+
+                if len(label.shape) != 4:  # only for 3D
+                    raise ValueError("Only supports label with shape CHWD!")
+
+                sids = self._apply(label, d)
+                if sids is not None and len(sids.keys()):
+                    d[self.sids] = sids
+                return d
+            else:
+                print("This transform only applies to label key")
+        return d
+
+
+class AddInitialSeedPointCustomd(Randomizable, MapTransform):
+    """
+    Add random guidance as initial seed point for a given label.
+    Note that the label is of size (C, D, H, W) or (C, H, W)
+    The guidance is of size (2, N, # of dims) where N is number of guidance added.
+    # of dims = 4 when C, D, H, W; # of dims = 3 when (C, H, W)
+    Args:
+        label: label source.
+        guidance: key to store guidance.
+        sids: key that represents lists of valid slice indices for the given label.
+        sid: key that represents the slice to add initial seed point.  If not present, random sid will be chosen.
+        connected_regions: maximum connected regions to use for adding initial points.
+    """
+
+    def __init__(
+        self,
+        keys: KeysCollection,
+        guidance: str = "guidance",
+        sids: str = "sids",
+        sid: str = "sid",
+        connected_regions: int = 5,
+        allow_missing_keys: bool = False,
+    ):
+        super().__init__(keys, allow_missing_keys)
+        self.sids_key = sids
+        self.sid_key = sid
+        self.sid: Dict[str, int] = dict()
+        self.guidance = guidance
+        self.connected_regions = connected_regions
+
+    def _apply(self, label, sid, key_label):
+        dimensions = 3 if len(label.shape) > 3 else 2
+        self.default_guidance = [-1] * (dimensions + 1)
+
+        dims = dimensions
+        if sid is not None and dimensions == 3:
+            dims = 2
+            label = label[0][..., sid][np.newaxis]  # Assume channel is first and depth is last CHWD
+
+        # import matplotlib.pyplot as plt
+        # plt.imshow(label[0])
+        # plt.title(f'label as is {key_label}')
+        # plt.show()
+        # plt.close()
+
+        # THERE MAY BE MULTIPLE BLOBS FOR SINGLE LABEL IN THE SELECTED SLICE
+        label = (label > 0.5).astype(np.float32)
+        # measure.label: Label connected regions of an integer array - Two pixels are connected
+        # when they are neighbors and have the same value
+        blobs_labels = measure.label(label.astype(int), background=0) if dims == 2 else label
+        if np.max(blobs_labels) <= 0:
+            raise AssertionError(f"SLICES NOT FOUND FOR LABEL: {key_label}")
+
+        # plt.imshow(blobs_labels[0])
+        # plt.title(f'Blobs {key_label}')
+        # plt.show()
+        # plt.close()
+
+        pos_guidance = []
+        for ridx in range(1, 2 if dims == 3 else self.connected_regions + 1):
+            if dims == 2:
+                label = (blobs_labels == ridx).astype(np.float32)
+                if np.sum(label) == 0:
+                    pos_guidance.append(self.default_guidance)
+                    continue
+
+            # plt.imshow(label[0])
+            # plt.title(f'Label postprocessed with blob number {key_label}')
+            # plt.show()
+
+            # plt.imshow(distance_transform_cdt(label)[0])
+            # plt.title(f'Transform CDT {key_label}')
+            # plt.show()
+
+            # The distance transform provides a metric or measure of the separation of points in the image.
+            # This function calculates the distance between each pixel that is set to off (0) and
+            # the nearest nonzero pixel for binary images - http://matlab.izmiran.ru/help/toolbox/images/morph14.html
+            distance = distance_transform_cdt(label).flatten()
+            probability = np.exp(distance) - 1.0
+
+            idx = np.where(label.flatten() > 0)[0]
+            seed = self.R.choice(idx, size=1, p=probability[idx] / np.sum(probability[idx]))
+            dst = distance[seed]
+
+            g = np.asarray(np.unravel_index(seed, label.shape)).transpose().tolist()[0]
+            g[0] = dst[0]  # for debug
+            if dimensions == 2 or dims == 3:
+                pos_guidance.append(g)
+            else:
+                # Clicks are created using this convention Channel Height Width Depth (CHWD)
+                pos_guidance.append([g[0], g[-2], g[-1], sid])  # Assume channel is first and depth is last CHWD
+
+        return np.asarray([pos_guidance])
+
+    def _randomize(self, d, key_label):
+        sids = d.get(self.sids_key, None).get(key_label, None) if d.get(self.sids_key, None) is not None else None
+        sid = d.get(self.sid_key, None).get(key_label, None) if d.get(self.sid_key, None) is not None else None
+        if sids is not None and sids:
+            if sid is None or sid not in sids:
+                sid = self.R.choice(sids, replace=False)
+        else:
+            logger.info(f"Not slice IDs for label: {key_label}")
+            sid = None
+        self.sid[key_label] = sid
+
+    def __call__(self, data: Mapping[Hashable, np.ndarray]) -> Dict[Hashable, np.ndarray]:
+        d: Dict = dict(data)
+        for key in self.key_iterator(d):
+            if key == "label":
+                label_guidances = {}
+                for key_label in d["sids"].keys():
+                    # Randomize: Select a random slice
+                    self._randomize(d, key_label)
+                    # Generate guidance base on selected slice
+                    tmp_label = np.copy(d[key])
+                    # Taking one label to create the guidance
+                    if key_label != "background":
+                        tmp_label[tmp_label != float(d["label_names"][key_label])] = 0
+                    else:
+                        tmp_label[tmp_label != float(d["label_names"][key_label])] = 1
+                        tmp_label = 1 - tmp_label
+                    label_guidances[key_label] = json.dumps(
+                        self._apply(tmp_label, self.sid.get(key_label, None), key_label).astype(int).tolist()
+                    )
+                d[self.guidance] = label_guidances
+                return d
+            else:
+                print("This transform only applies to label key")
+        return d
+
+
 class FindDiscrepancyRegionsCustomd(MapTransform):
     """
     Find discrepancy between prediction and actual during click interactions during training.
