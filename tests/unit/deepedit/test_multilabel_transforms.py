e31a671b889e2a0c53e7389bfa40d784d1205150
Andres Diaz-Pinto <diazandr3s@gmail.com>
2022-04-27 11:43:00 +0100

    Add/Update all unit tests for DeepEdit transforms (#759)
    * Add/Update all unit tests for DeepEdit transforms
    
    Signed-off-by: Andres Diaz-Pinto <diazandr3s@gmail.com>
    
    * Fix Postfix error
    
    Signed-off-by: Andres Diaz-Pinto <diazandr3s@gmail.com>
    
    * Recover deleted transforms
    
    Signed-off-by: Andres Diaz-Pinto <diazandr3s@gmail.com>

--- tests/unit/deepedit/test_multilabel_transforms.py
+++ tests/unit/deepedit/test_multilabel_transforms.py
@@ -15,127 +15,52 @@ import numpy as np
 from parameterized import parameterized
 
 from monailabel.deepedit.multilabel.transforms import (
+    AddGuidanceFromPointsCustomd,
     AddGuidanceSignalCustomd,
+    AddInitialSeedPointMissingLabelsd,
+    AddRandomGuidanceCustomd,
     DiscardAddGuidanced,
+    FindAllValidSlicesMissingLabelsd,
+    FindDiscrepancyRegionsCustomd,
     NormalizeLabelsInDatasetd,
+    ResizeGuidanceMultipleLabelCustomd,
     SingleLabelSelectiond,
     SplitPredsLabeld,
     ToCheckTransformd,
 )
 
-IMAGE = np.array([[[[1, 0, 2, 0, 1], [0, 1, 2, 1, 0], [2, 2, 3, 2, 2], [0, 1, 2, 1, 0], [1, 0, 2, 0, 1]]]])
-LABEL = np.array([[[[0, 0, 0, 0, 0], [0, 1, 0, 1, 0], [0, 0, 1, 0, 0], [0, 1, 0, 1, 0], [0, 0, 0, 0, 0]]]])
-MULTIMODALITY_IMAGE = np.random.rand(5, 5, 5)
-MULTI_LABEL = np.random.randint(0, 6, (5, 5))
-PRED = np.random.randint(0, 6, (5, 5))
-LABEL_NAMES = {
-    "spleen": 1,
-    "right kidney": 2,
-    "background": 0,
+np.random.seed(0)
+IMAGE = np.random.randint(0, 256, size=(1, 10, 10, 10))
+THREE_CHAN_IMAGE = np.random.randint(0, 255, size=(3, 10, 10, 10))
+LABEL = np.random.randint(0, 2, size=(10, 10, 10))
+PRED = np.random.randint(0, 2, size=(10, 10, 10))
+LABEL_NAMES = {"spleen": 1, "background": 0}
+DISCREPANCY = {
+    "spleen": np.random.randint(0, 2, size=(10, 10, 10)),
+    "background": np.random.randint(0, 2, size=(10, 10, 10)),
 }
 
 DATA_1 = {
     "image": IMAGE,
     "label": LABEL,
-    "image_meta_dict": {
-        "dim": np.array(IMAGE.shape),
-        "pixdim": [1, 1, 1, 5, 1, 1, 1, 1],
-        "filename_or_obj": "IMAGE_NAME",
-    },
+    "image_meta_dict": {"dim": IMAGE.shape, "spatial_shape": IMAGE[0, ...].shape},
     "label_meta_dict": {},
-    "foreground": [0, 0, 0],
-    "background": [0, 0, 0],
 }
-
-DISCARD_ADD_GUIDANCE_TEST_CASE = [
-    DATA_1,
-    4,
-]
 
 DATA_2 = {
     "image": IMAGE,
     "label": LABEL,
-    "guidance": np.array([[[1, 0, 2, 2]], [[-1, -1, -1, -1]]]),
-    "discrepancy": np.array(
-        [
-            [[[[0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]]],
-            [[[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]]],
-        ]
-    ),
+    "label_names": LABEL_NAMES,
+    "guidance": {"spleen": [[3, 5, 4, 6], [-1, -1, -1, -1]], "background": [[-1, -1, -1, -1], [-1, -1, -1, -1]]},
+    "discrepancy": DISCREPANCY,
     "probability": 1.0,
 }
 
 DATA_3 = {
-    "image": np.arange(1000).reshape((1, 5, 10, 20)),
-    "image_meta_dict": {
-        "foreground_cropped_shape": (1, 10, 20, 40),
-        "dim": [3, 512, 512, 128],
-        "spatial_shape": [512, 512, 128],
-    },
-    "guidance": [[[6, 10, 14], [8, 10, 14]], [[8, 10, 16]]],
-    "foreground": [[10, 14, 6], [10, 14, 8]],
-    "background": [[10, 16, 8]],
-}
-
-DATA_4 = {
-    "image": MULTIMODALITY_IMAGE,
-    "label": MULTI_LABEL,
-    "image_meta_dict": {
-        "dim": [1, 1, 1, 5, 2, 1, 1, 1],
-        "pixdim": [1, 1, 1, 5, 2, 1, 1, 1],
-        "filename_or_obj": "IMAGE_NAME",
-    },
-    "label_meta_dict": {
-        "dim": [1, 1, 1, 5, 2, 1, 1, 1],
-        "pixdim": [1, 1, 1, 5, 2, 1, 1, 1],
-        "filename_or_obj": "LABEL_NAME",
-    },
-}
-
-DATA_5 = {
     "image": IMAGE,
-    "label": MULTI_LABEL,
-    "guidance": {
-        "spleen": np.array([[[1, 0, 2, 2], [-1, -1, -1, -1]]]),
-        "right kidney": np.array([[[1, 0, 2, 2], [-1, -1, -1, -1]]]),
-        "background": np.array([[[1, 0, 2, 2], [-1, -1, -1, -1]]]),
-    },
-    "discrepancy": {
-        "spleen": np.array(
-            [
-                [[[[0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]]],
-                [[[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]]],
-            ]
-        ),
-        "right kidney": np.array(
-            [
-                [[[[0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]]],
-                [[[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]]],
-            ]
-        ),
-        "background": np.array(
-            [
-                [[[[0, 0, 0, 0, 0], [0, 0, 0, 1, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]]],
-                [[[[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]]],
-            ]
-        ),
-    },
-    "probability": 1.0,
-    "label_names": LABEL_NAMES,
-}
-
-PosNegClickProbAddRandomGuidanceCustomd_TEST_CASE = [
-    {"guidance": "guidance", "discrepancy": "discrepancy", "probability": "probability"},
-    DATA_5,
-    4,
-]
-
-DATA_6 = {
-    "image": IMAGE,
-    "label": MULTI_LABEL,
+    "label": LABEL,
     "guidance": {
         "spleen": np.array([[1, 0, 2, 2], [-1, -1, -1, -1]]),
-        "right kidney": np.array([[1, 0, 2, 2], [-1, -1, -1, -1]]),
         "background": np.array([[1, 0, 2, 2], [-1, -1, -1, -1]]),
     },
     "probability": 1.0,
@@ -143,158 +68,260 @@ DATA_6 = {
     "pred": PRED,
 }
 
-FindDiscrepancyRegionsCustomd_TEST_CASE = [
-    {"discrepancy": "discrepancy"},
-    DATA_6,
-    (5, 5),
-]
+DATA_4 = {
+    "image": IMAGE,
+    "label": LABEL,
+    "image_meta_dict": {"dim": IMAGE.shape, "spatial_shape": IMAGE[0, ...].shape},
+    "current_label": "spleen",
+    "probability": 1.0,
+    "label_names": LABEL_NAMES,
+    "spleen": [[0, 4, 3], [0, 0, 3], [0, 1, 3]],
+    "sids": {"spleen": []},
+    "pred": PRED,
+}
 
-NormalizeLabelsDatasetd_TEST_CASE = [
-    {"label_names": LABEL_NAMES},
-    DATA_6,
-    len(LABEL_NAMES),
-]
+DATA_5 = {
+    "image": IMAGE,
+    "label": LABEL,
+    "image_meta_dict": {"dim": IMAGE.shape, "spatial_shape": IMAGE[0, ...].shape},
+    "current_label": "spleen",
+    "probability": 1.0,
+    "label_names": LABEL_NAMES,
+    "sids": {"spleen": [2, 3, 4], "background": [0, 1, 5]},
+}
 
-ADD_GUIDANCE_CUSTOM_TEST_CASE = [
-    DATA_6,
-    4,
-]
-
+DATA_6 = {
+    "image": IMAGE,
+    "label": LABEL[None],
+    "image_meta_dict": {"dim": IMAGE.shape, "spatial_shape": IMAGE[0, ...].shape},
+    "current_label": "spleen",
+    "label_names": LABEL_NAMES,
+}
 
 DATA_7 = {
     "image": IMAGE,
-    "label": MULTI_LABEL,
+    "label": LABEL,
+    "pred": PRED,
+    "image_meta_dict": {"dim": IMAGE.shape, "spatial_shape": IMAGE[0, ...].shape},
     "current_label": "spleen",
     "probability": 1.0,
     "label_names": LABEL_NAMES,
-    "pred": PRED,
+    "guidance": {
+        "spleen": np.array([[1, 0, 2, 2], [-1, -1, -1, -1]]),
+        "background": np.array([[1, 0, 2, 2], [-1, -1, -1, -1]]),
+    },
 }
-
-SingleLabelSelectiond_TEST_CASE = [
-    {"label_names": ["spleen"]},
-    DATA_7,
-    "spleen",
-]
 
 DATA_8 = {
     "image": IMAGE,
-    "label": MULTI_LABEL,
-    "current_label": "spleen",
-    "probability": 1.0,
+    "label": LABEL,
+    "image_meta_dict": {"dim": IMAGE.shape, "spatial_shape": IMAGE[0, ...].shape},
     "label_names": LABEL_NAMES,
-    "pred": PRED,
 }
-
-SplitPredsLabeld_TEST_CASE = [DATA_7]
-
-ToCheckTransformd_TEST_CASE = [DATA_7, 6]
-
-SingleModalityLabelSanityd_TEST_CASE = [DATA_7, (5, 5)]
-
 
 DATA_9 = {
     "image": IMAGE,
-    "label": MULTI_LABEL,
-    "current_label": "spleen",
-    "probability": 1.0,
+    "label": LABEL,
+    "image_meta_dict": {"dim": IMAGE.shape, "spatial_shape": IMAGE[0, ...].shape},
     "label_names": LABEL_NAMES,
-    "pred": PRED,
-    "sids": {"spleen": [1, 2, 3]},
+    "guidance": {
+        "spleen": np.array([0, 2, 2]),
+        "background": np.array([-1, -1, -1]),
+    },
 }
 
-ADD_INITIAL_POINT_TEST_CASE_1 = [
-    {"guidance": "guidance", "sids": "sids"},
-    DATA_9,
-    "[[[1, 0], [1, 2], [1, 0], [1, 0], [1, 2]]]",
+DATA_10 = {
+    "image": IMAGE,
+    "label": LABEL,
+    "image_meta_dict": {"dim": IMAGE.shape, "spatial_shape": IMAGE[0, ...].shape},
+    "current_label": "spleen",
+}
+
+DATA_11 = {
+    "image": IMAGE,
+    "label": LABEL,
+    "label_names": LABEL_NAMES,
+    "pred": PRED,
+}
+
+DATA_12 = {
+    "image": IMAGE,
+    "label": LABEL,
+    "label_names": LABEL_NAMES,
+}
+
+ADD_GUIDANCE_FROM_POINTS_TEST_CASE = [
+    {"ref_image": "image", "guidance": "guidance", "label_names": LABEL_NAMES},  # arguments
+    DATA_4,  # input_data
+    [0, 4, 3],  # expected_result
 ]
 
-# When checking tensor content use np.testing.assert_equal(result["image"], expected_values)
+ADD_GUIDANCE_CUSTOM_TEST_CASE = [
+    {"keys": "image", "guidance": "guidance"},  # arguments
+    DATA_3,  # input_data
+    3,  # expected_result
+]
+
+ADD_INITIAL_POINT_TEST_CASE = [
+    {"keys": "label", "guidance": "guidance", "sids": "sids"},  # arguments
+    DATA_5,  # input_data
+    {
+        "spleen": "[[1, 0, 7], [-1, -1, -1], [-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]",
+        "background": "[[1, 5, 3], [-1, -1, -1], [-1, -1, -1], [-1, -1, -1], [-1, -1, -1]]",
+    },  # expected_result
+]
+
+ADD_RANDOM_GUIDANCE_TEST_CASE = [
+    {"keys": "NA", "guidance": "guidance", "discrepancy": "discrepancy", "probability": "probability"},  # arguments
+    DATA_2,  # input_data
+    {"spleen": [[3, 5, 4, 6], [-1, -1, -1, -1]], "background": [[-1, -1, -1, -1], [-1, -1, -1, -1]]},  # expected_result
+]
+
+DISCARD_ADD_GUIDANCE_TEST_CASE = [
+    {"keys": "image", "label_names": LABEL_NAMES},  # arguments
+    DATA_1,  # input_data
+    (3, 10, 10, 10),  # expected_result
+]
+
+FIND_DISCREPANCY_TEST_CASE = [
+    {"keys": "label", "pred": "pred", "discrepancy": "discrepancy"},  # arguments
+    DATA_7,  # input_data
+    240,  # expected_result
+]
+
+FIND_SLICE_TEST_CASE = [
+    {"keys": "label", "sids": "sids"},  # arguments
+    DATA_6,  # input_data
+    {"spleen": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9], "background": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]},  # expected_result
+]
+
+NormalizeLabelsDatasetd_TEST_CASE = [
+    {"keys": "label", "label_names": LABEL_NAMES},  # arguments
+    DATA_8,  # input_data
+    len(LABEL_NAMES),  # expected_result
+]
+
+RESIZE_GUIDANCE_TEST_CASE = [
+    {"guidance": "guidance", "ref_image": "image"},  # arguments
+    DATA_9,  # input_data
+    {"spleen": [0, 2, 2], "background": [-1, -1, -1]},  # expected_result
+]
+
+SingleLabelSelectiond_TEST_CASE = [
+    {"keys": "label", "label_names": ["spleen"]},  # arguments
+    DATA_10,  # input_data
+    "spleen",  # expected_result
+]
+
+SplitPredsLabeld_TEST_CASE = [
+    {"keys": "pred"},  # arguments
+    DATA_11,  # input_data
+    (1, 10, 10),  # expected_result
+]
+
+ToCheckTransformd_TEST_CASE = [
+    {"keys": "image"},  # arguments
+    DATA_12,  # input_data
+    (1, 10, 10, 10),  # expected_result
+]
+
+
+class TestAddGuidanceFromPointsCustomd(unittest.TestCase):
+    @parameterized.expand([ADD_GUIDANCE_FROM_POINTS_TEST_CASE])
+    def test_correct_results(self, arguments, input_data, expected_result):
+        add_fn = AddGuidanceFromPointsCustomd(**arguments)
+        result = add_fn(input_data)
+        self.assertEqual(result[arguments["guidance"]]["spleen"][0], expected_result)
+
+
+class TestAddGuidanceSignalCustomd(unittest.TestCase):
+    @parameterized.expand([ADD_GUIDANCE_CUSTOM_TEST_CASE])
+    def test_correct_results(self, arguments, input_data, expected_result):
+        add_fn = AddGuidanceSignalCustomd(**arguments)
+        result = add_fn(input_data)
+        self.assertEqual(result["image"].shape[0], expected_result)
+
+
+class TestAddInitialSeedPointMissingLabelsd(unittest.TestCase):
+    @parameterized.expand([ADD_INITIAL_POINT_TEST_CASE])
+    def test_correct_results(self, arguments, input_data, expected_result):
+        seed = 0
+        add_fn = AddInitialSeedPointMissingLabelsd(**arguments)
+        add_fn.set_random_state(seed)
+        result = add_fn(input_data)
+        self.assertEqual(result[arguments["guidance"]], expected_result)
+
+
+class TestAddRandomGuidanceCustomd(unittest.TestCase):
+    @parameterized.expand([ADD_RANDOM_GUIDANCE_TEST_CASE])
+    def test_correct_results(self, arguments, input_data, expected_result):
+        add_fn = AddRandomGuidanceCustomd(**arguments)
+        result = add_fn(input_data)
+        self.assertEqual(result[arguments["guidance"]], expected_result)
 
 
 class TestDiscardAddGuidanced(unittest.TestCase):
     @parameterized.expand([DISCARD_ADD_GUIDANCE_TEST_CASE])
-    def test_correct_results(self, input_data, expected_result):
-        add_fn = DiscardAddGuidanced(keys="image", label_names=LABEL_NAMES)
+    def test_correct_results(self, arguments, input_data, expected_result):
+        add_fn = DiscardAddGuidanced(**arguments)
         result = add_fn(input_data)
-        np.testing.assert_equal(result["image"].shape[0], expected_result)
+        self.assertEqual(result["image"].shape, expected_result)
+
+
+class TestFindAllValidSlicesMissingLabelsd(unittest.TestCase):
+    @parameterized.expand([FIND_SLICE_TEST_CASE])
+    def test_correct_results(self, arguments, input_data, expected_result):
+        add_fn = FindAllValidSlicesMissingLabelsd(**arguments)
+        result = add_fn(input_data)
+        self.assertEqual(result[arguments["sids"]], expected_result)
+
+
+class TestFindDiscrepancyRegionsCustomd(unittest.TestCase):
+    @parameterized.expand([FIND_DISCREPANCY_TEST_CASE])
+    def test_correct_results(self, arguments, input_data, expected_result):
+        add_fn = FindDiscrepancyRegionsCustomd(**arguments)
+        result = add_fn(input_data)
+        self.assertEqual(np.sum(result[arguments["discrepancy"]]["spleen"][0]), expected_result)
 
 
 class TestNormalizeLabelsDatasetd(unittest.TestCase):
     @parameterized.expand([NormalizeLabelsDatasetd_TEST_CASE])
     def test_correct_results(self, arguments, input_data, expected_result):
-        add_fn = NormalizeLabelsInDatasetd(keys="label", **arguments)
+        add_fn = NormalizeLabelsInDatasetd(**arguments)
         result = add_fn(input_data)
         self.assertEqual(len(np.unique(result["label"])), expected_result)
+
+
+class TestResizeGuidanceMultipleLabelCustomd(unittest.TestCase):
+    @parameterized.expand([RESIZE_GUIDANCE_TEST_CASE])
+    def test_correct_results(self, arguments, input_data, expected_result):
+        add_fn = ResizeGuidanceMultipleLabelCustomd(**arguments)
+        result = add_fn(input_data)
+        self.assertEqual(result[arguments["guidance"]], expected_result)
 
 
 class TestSingleLabelSelectiond(unittest.TestCase):
     @parameterized.expand([SingleLabelSelectiond_TEST_CASE])
     def test_correct_results(self, arguments, input_data, expected_result):
-        add_fn = SingleLabelSelectiond(keys="label", **arguments)
+        add_fn = SingleLabelSelectiond(**arguments)
         result = add_fn(input_data)
         self.assertEqual(result["current_label"], expected_result)
 
 
-class TestAddGuidanceSignalCustomd(unittest.TestCase):
-    @parameterized.expand([ADD_GUIDANCE_CUSTOM_TEST_CASE])
-    def test_correct_results(self, input_data, expected_result):
-        add_fn = AddGuidanceSignalCustomd(keys="image")
-        result = add_fn(input_data)
-        self.assertEqual(result["image"].shape[0], expected_result)
-
-
 class TestSplitPredsLabeld(unittest.TestCase):
     @parameterized.expand([SplitPredsLabeld_TEST_CASE])
-    def test_correct_results(self, input_data):
-        add_fn = SplitPredsLabeld(keys="pred")
+    def test_correct_results(self, arguments, input_data, expected_result):
+        add_fn = SplitPredsLabeld(**arguments)
         result = add_fn(input_data)
-        self.assertIsNotNone(result["pred_spleen"])
+        self.assertEqual(result["pred_spleen"].shape, expected_result)
 
 
-# Simple transform to debug other transforms
 class TestToCheckTransformd(unittest.TestCase):
     @parameterized.expand([ToCheckTransformd_TEST_CASE])
-    def test_correct_results(self, input_data, expected_result):
-        add_fn = ToCheckTransformd(keys="label")
+    def test_correct_results(self, arguments, input_data, expected_result):
+        add_fn = ToCheckTransformd(**arguments)
         result = add_fn(input_data)
-        self.assertEqual(len(result), expected_result)
-
-
-# # WORK IN PROGRESS
-# class TestPosNegClickProbAddRandomGuidanceCustomd(unittest.TestCase):
-#     @parameterized.expand([PosNegClickProbAddRandomGuidanceCustomd_TEST_CASE])
-#     def test_correct_results(self, arguments, input_data, expected_result):
-#         seed = 0
-#         add_fn = PosNegClickProbAddRandomGuidanceCustomd(keys="NA", **arguments)
-#         add_fn.set_random_state(seed)
-#         result = add_fn(input_data)
-#         self.assertGreaterEqual(len(result[arguments["guidance"]]["spleen"]), expected_result)
-#
-#
-# class TestFindDiscrepancyRegionsCustomd(unittest.TestCase):
-#     @parameterized.expand([FindDiscrepancyRegionsCustomd_TEST_CASE])
-#     def test_correct_results(self, arguments, input_data, expected_result):
-#         add_fn = FindDiscrepancyRegionsCustomd(keys="label", **arguments)
-#         result = add_fn(input_data)
-#         self.assertEqual(result["discrepancy"]["spleen"][0].shape, expected_result)
-#
-#
-# class TestSingleModalityLabelSanityd(unittest.TestCase):
-#     @parameterized.expand([SingleModalityLabelSanityd_TEST_CASE])
-#     def test_correct_results(self, input_data, expected_result):
-#         add_fn = SingleModalityLabelSanityd(keys="label")
-#         result = add_fn(input_data)
-#         self.assertEqual(result["label"].shape, expected_result)
-#
-#
-# class TestAddInitialSeedPointd(unittest.TestCase):
-#     @parameterized.expand([ADD_INITIAL_POINT_TEST_CASE_1])
-#     def test_correct_results(self, arguments, input_data, expected_result):
-#         seed = 0
-#         add_fn = AddInitialSeedPointCustomd(keys='label', **arguments)
-#         add_fn.set_random_state(seed)
-#         result = add_fn(input_data)
-#         self.assertEqual(result['guidance']['background'], expected_result)
+        self.assertEqual(result["image"].shape, expected_result)
 
 
 if __name__ == "__main__":
