683566bf2a2800c6c28e4ff833eca72510845915
SACHIDANAND ALLE <sachidanand.alle@gmail.com>
2022-04-28 15:21:19 +0100

    nuclick infer model - draft
    Signed-off-by: SACHIDANAND ALLE <sachidanand.alle@gmail.com>

---
+++ sample-apps/pathology/lib/configs/nuclick.py
@@ -0,0 +1,71 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+import logging
+import os
+from distutils.util import strtobool
+from typing import Any, Dict, Optional, Union
+
+import lib.infers
+import lib.trainers
+from monai.networks.nets import BasicUNet
+
+from monailabel.interfaces.config import TaskConfig
+from monailabel.interfaces.tasks.infer import InferTask
+from monailabel.interfaces.tasks.train import TrainTask
+from monailabel.utils.others.generic import device_list, download_file
+
+logger = logging.getLogger(__name__)
+
+
+class NuClick(TaskConfig):
+    def init(self, name: str, model_dir: str, conf: Dict[str, str], planner: Any, **kwargs):
+        super().init(name, model_dir, conf, planner, **kwargs)
+
+        # Labels
+        self.labels = ["Nuclei"]
+        self.label_colors = {"Nuclei": (0, 255, 255)}
+
+        # Model Files
+        self.path = [
+            os.path.join(self.model_dir, f"pretrained_{name}.pt"),  # pretrained
+            os.path.join(self.model_dir, f"{name}.pt"),  # published
+        ]
+
+        # Download PreTrained Model
+        if strtobool(self.conf.get("use_pretrained_model", "true")):
+            url = f"{self.PRE_TRAINED_PATH}/NuClick_UNet_40xAll.pth"
+            download_file(url, self.path[0])
+
+        # Network
+        self.network = BasicUNet(
+            spatial_dims=2,
+            in_channels=5,
+            out_channels=1,
+            features=(32, 64, 128, 256, 512, 32),
+        )
+
+    def infer(self) -> Union[InferTask, Dict[str, InferTask]]:
+        task: InferTask = lib.infers.NuClick(
+            path=self.path,
+            network=self.network,
+            labels=self.labels,
+            preload=strtobool(self.conf.get("preload", "false")),
+            roi_size=json.loads(self.conf.get("roi_size", "[512, 512]")),
+            config={
+                "label_colors": self.label_colors,
+                "max_workers": len(device_list()),
+            },
+        )
+        return task
+
+    def trainer(self) -> Optional[TrainTask]:
+        return None
