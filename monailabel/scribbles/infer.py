64367b237b6f04562012fd3690c32ba9d7e779ff
masadcv <muhammad.asad@kcl.ac.uk>
2022-04-29 17:14:32 +0100

    adding GMM based scribbles segmentor
    Signed-off-by: masadcv <muhammad.asad@kcl.ac.uk>

--- monailabel/scribbles/infer.py
+++ monailabel/scribbles/infer.py
@@ -16,6 +16,7 @@ from monailabel.scribbles.transforms import (
     AddBackgroundScribblesFromROId,
     ApplyGraphCutOptimisationd,
     MakeISegUnaryd,
+    MakeLikelihoodFromScribblesGMMd,
     MakeLikelihoodFromScribblesHistogramd,
 )
 from monailabel.transform.post import BoundingBoxd, Restored
@@ -44,13 +45,14 @@ class HistogramBasedGraphCut(InferTask):
         pix_dim=(2.5, 2.5, 5.0),
         lamda=1.0,
         sigma=0.1,
+        num_bins=64,
         labels=None,
         config=None,
     ):
         if config:
-            config.update({"lamda": lamda, "sigma": sigma})
+            config.update({"lamda": lamda, "sigma": sigma, "num_bins": num_bins})
         else:
-            config = {"lamda": lamda, "sigma": sigma}
+            config = {"lamda": lamda, "sigma": sigma, "num_bins": num_bins}
         super().__init__(
             path=None,
             network=None,
@@ -64,6 +66,7 @@ class HistogramBasedGraphCut(InferTask):
         self.pix_dim = pix_dim
         self.lamda = lamda
         self.sigma = sigma
+        self.num_bins = num_bins
 
         # set default scribbles labels
         self.scribbles_bg_label = 2 if not self.labels else len(self.labels) + 1
@@ -96,7 +99,122 @@ class HistogramBasedGraphCut(InferTask):
                 post_proc_label="prob",
                 scribbles_bg_label=self.scribbles_bg_label,
                 scribbles_fg_label=self.scribbles_fg_label,
+                num_bins=self.num_bins,
                 normalise=True,
+            ),
+        ]
+
+    def inferer(self, data):
+        return Compose(
+            [
+                # unary term maker
+                MakeISegUnaryd(
+                    image="image",
+                    logits="prob",
+                    scribbles="label",
+                    unary="unary",
+                    scribbles_bg_label=self.scribbles_bg_label,
+                    scribbles_fg_label=self.scribbles_fg_label,
+                ),
+                # optimiser
+                ApplyGraphCutOptimisationd(
+                    unary="unary",
+                    pairwise="image",
+                    post_proc_label="pred",
+                    lamda=self.lamda,
+                    sigma=self.sigma,
+                ),
+            ]
+        )
+
+    def post_transforms(self, data):
+        return [
+            Restored(keys="pred", ref_image="image"),
+            BoundingBoxd(keys="pred", result="result", bbox="bbox"),
+        ]
+
+
+class GMMBasedGraphCut(InferTask):
+    """
+    Defines Gaussian Mixture Model (GMM) based task for Generic segmentation from the following papers:
+
+    Rother, Carsten, Vladimir Kolmogorov, and Andrew Blake. "" GrabCut" interactive foreground extraction using iterated graph cuts."
+    ACM transactions on graphics (TOG) 23.3 (2004): 309-314.
+
+    Wang, Guotai, et al. "Interactive medical image segmentation using deep learning with image-specific fine tuning."
+    IEEE transactions on medical imaging 37.7 (2018): 1562-1573. (preprint: https://arxiv.org/pdf/1710.04043.pdf)
+
+    This task takes as input 1) original image volume and 2) scribbles from user
+    indicating foreground and background regions. A likelihood volume is generated using GMM method.
+    User-scribbles are incorporated using Equation 7 on page 4 from Guotai et al.
+
+    numpymaxflow's GraphCut layer is used to optimise Equation 5 from Guotai et al., where unaries come from Equation 7
+    and pairwise is the original input volume.
+    """
+
+    def __init__(
+        self,
+        dimension=3,
+        description="A post processing step with GMM-based GraphCut for Generic segmentation",
+        intensity_range=(-300, 200, 0.0, 1.0, True),
+        pix_dim=(2.5, 2.5, 5.0),
+        lamda=1.0,
+        sigma=0.1,
+        mixture_size=20,
+        labels=None,
+        config=None,
+    ):
+        if config:
+            config.update({"lamda": lamda, "sigma": sigma, "mixture_size": mixture_size})
+        else:
+            config = {"lamda": lamda, "sigma": sigma, "mixture_size": mixture_size}
+        super().__init__(
+            path=None,
+            network=None,
+            labels=labels,
+            type=InferType.SCRIBBLES,
+            dimension=dimension,
+            description=description,
+            config=config,
+        )
+        self.intensity_range = intensity_range
+        self.pix_dim = pix_dim
+        self.lamda = lamda
+        self.sigma = sigma
+        self.mixture_size = mixture_size
+
+        # set default scribbles labels
+        self.scribbles_bg_label = 2 if not self.labels else len(self.labels) + 1
+        self.scribbles_fg_label = 3 if not self.labels else len(self.labels) + 2
+
+    def pre_transforms(self, data):
+        return [
+            LoadImaged(keys=["image", "label"]),
+            EnsureChannelFirstd(keys=["image", "label"]),
+            AddBackgroundScribblesFromROId(
+                scribbles="label",
+                scribbles_bg_label=self.scribbles_bg_label,
+                scribbles_fg_label=self.scribbles_fg_label,
+            ),
+            # at the moment optimisers are bottleneck taking a long time,
+            # therefore scaling non-isotropic with big spacing
+            Spacingd(keys=["image", "label"], pixdim=self.pix_dim, mode=["bilinear", "nearest"]),
+            Orientationd(keys=["image", "label"], axcodes="RAS"),
+            ScaleIntensityRanged(
+                keys="image",
+                a_min=self.intensity_range[0],
+                a_max=self.intensity_range[1],
+                b_min=self.intensity_range[2],
+                b_max=self.intensity_range[3],
+                clip=self.intensity_range[4],
+            ),
+            MakeLikelihoodFromScribblesGMMd(
+                image="image",
+                scribbles="label",
+                post_proc_label="prob",
+                scribbles_bg_label=self.scribbles_bg_label,
+                scribbles_fg_label=self.scribbles_fg_label,
+                normalise=False,
             ),
         ]
 
--- monailabel/scribbles/utils.py
+++ monailabel/scribbles/utils.py
@@ -12,6 +12,9 @@ import logging
 
 import numpy as np
 import numpymaxflow
+import torch
+from monai.networks.layers import GaussianMixtureModel
+from sklearn.mixture import GaussianMixture
 
 logger = logging.getLogger(__name__)
 
@@ -137,7 +140,9 @@ def make_histograms(image, scrib, scribbles_bg_label, scribbles_fg_label, alpha_
     return bg_hist.astype(np.float32), fg_hist.astype(np.float32), fg_bin_edges.astype(np.float32)
 
 
-def make_likelihood_image_histogram(image, scrib, scribbles_bg_label, scribbles_fg_label, return_label=False):
+def make_likelihood_image_histogram(
+    image, scrib, scribbles_bg_label, scribbles_fg_label, num_bins=64, return_label=False
+):
     # normalise image in range [0, 1] if needed
     min_img = np.min(image)
     max_img = np.max(image)
@@ -146,7 +151,7 @@ def make_likelihood_image_histogram(image, scrib, scribbles_bg_label, scribbles_
 
     # generate histograms for background/foreground
     bg_hist, fg_hist, bin_edges = make_histograms(
-        image, scrib, scribbles_bg_label, scribbles_fg_label, alpha_bg=1, alpha_fg=1, bins=64
+        image, scrib, scribbles_bg_label, scribbles_fg_label, alpha_bg=1, alpha_fg=1, bins=num_bins
     )
 
     # lookup values for each voxel for generating background/foreground probabilities
@@ -154,6 +159,121 @@ def make_likelihood_image_histogram(image, scrib, scribbles_bg_label, scribbles_
     fprob = fg_hist[dimage]
     bprob = bg_hist[dimage]
     retprob = np.concatenate([bprob, fprob], axis=0)
+
+    # if needed, convert to discrete labels instead of probability
+    if return_label:
+        retprob = np.expand_dims(np.argmax(retprob, axis=0), axis=0).astype(np.float32)
+
+    return retprob
+
+
+def sklearn_fit_gmm(image, scrib, scribbles_bg_label, scribbles_fg_label, n_components):
+    bg = GaussianMixture(n_components=n_components)
+    bg.fit(image[scrib == scribbles_bg_label].reshape(-1, 1))
+
+    fg = GaussianMixture(n_components=n_components)
+    fg.fit(image[scrib == scribbles_fg_label].reshape(-1, 1))
+
+    return bg, fg
+
+
+def learn_and_apply_gmm_sklearn(image, scrib, scribbles_bg_label, scribbles_fg_label, mixture_size):
+    # based on https://github.com/jiviteshjain/grabcut/blob/main/src/grabcut.ipynb
+    bg_gmm, fg_gmm = sklearn_fit_gmm(image, scrib, scribbles_bg_label, scribbles_fg_label, mixture_size)
+
+    # add empty channel to image and scrib to be inline with pytorch layout
+    bg_prob = bg_gmm.score_samples(image.reshape((-1, 1))).reshape(image.shape).astype(np.float32)
+    fg_prob = fg_gmm.score_samples(image.reshape((-1, 1))).reshape(image.shape).astype(np.float32)
+
+    bg_prob = np.exp(bg_prob)
+    fg_prob = np.exp(fg_prob)
+
+    gmm_output = np.concatenate([bg_prob, fg_prob])
+
+    return gmm_output
+
+
+def learn_and_apply_gmm_monai(image, scrib, scribbles_bg_label, scribbles_fg_label, mixture_size):
+    # this function is limited to binary segmentation at the moment
+    n_classes = 2
+
+    # make trimap
+    trimap = np.zeros_like(scrib).astype(np.int32)
+
+    # fetch anything that is not scribbles
+    not_scribbles = ~((scrib == scribbles_bg_label) | (scrib == scribbles_fg_label))
+
+    # set these to 0 == unused
+    trimap[not_scribbles] = -1
+
+    # set background scrib to -1
+    trimap[scrib == scribbles_bg_label] = 0
+    # set foreground scrib to 1
+    trimap[scrib == scribbles_fg_label] = 1
+
+    # image_tri = image[~not_scribbles]
+    # trimap = trimap[~not_scribbles]
+
+    # add empty channel to image and scrib to be inline with pytorch layout
+    image = np.expand_dims(image, axis=0)
+    # image_tri = np.expand_dims(image_tri, axis=0)
+    trimap = np.expand_dims(trimap, axis=0)
+
+    # transfer everything to pytorch tensor,
+    # we only use CUDA as GMM from MONAI is only available on CUDA atm (2021/04/29)
+    # if no cuda device found, then exit now
+    if not torch.cuda.is_available():
+        raise OSError("Unable to find CUDA device, check your torch/monai installation")
+
+    device = "cuda"
+    image = torch.from_numpy(image).type(torch.float32).to(device)
+    trimap = torch.from_numpy(trimap).type(torch.int32).to(device)
+
+    # initialise our GMM
+    gmm = GaussianMixtureModel(
+        image.size(1),
+        mixture_count=n_classes,
+        mixture_size=mixture_size,
+        verbose_build=False,
+    )
+    # gmm.reset()
+
+    # learn gmm from image_tri and trimap
+    gmm.learn(image, trimap)
+
+    # apply gmm on image
+    gmm_output = gmm.apply(image)
+
+    return gmm_output.squeeze(0).cpu().numpy()
+
+
+def make_likelihood_image_gmm(
+    image,
+    scrib,
+    scribbles_bg_label,
+    scribbles_fg_label,
+    mixture_size=20,
+    return_label=False,
+):
+    # learn gmm and apply to image, return output label prob
+    try:
+        # this may fail if MONAI Cpp Extensions are not loaded properly
+        retprob = learn_and_apply_gmm_monai(
+            image=image,
+            scrib=scrib,
+            scribbles_bg_label=scribbles_bg_label,
+            scribbles_fg_label=scribbles_fg_label,
+            mixture_size=mixture_size,
+        )
+    except:
+        logging.info("Unable to run MONAI's GMM, falling back to sklearn GMM")
+        retprob = learn_and_apply_gmm_sklearn(
+            image=image,
+            scrib=scrib,
+            scribbles_bg_label=scribbles_bg_label,
+            scribbles_fg_label=scribbles_fg_label,
+            mixture_size=mixture_size,
+        )
 
     # if needed, convert to discrete labels instead of probability
     if return_label:
