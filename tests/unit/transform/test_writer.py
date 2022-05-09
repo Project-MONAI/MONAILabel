b8a5f81fc980afada5d918d3e9874d675ce76b9d
Markus Hinsche <m.hinsche@gmail.com>
2022-05-09 17:38:55 +0200

    Add unit test for NRRD writer
    Signed-off-by: Markus Hinsche <m.hinsche@gmail.com>

--- tests/unit/transform/test_writer.py
+++ tests/unit/transform/test_writer.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 import unittest
+import nrrd
 
 import numpy as np
 from parameterized import parameterized
@@ -18,6 +19,12 @@ WRITER_DATA = [
     },
 ]
 
+COLOR_MAP = {
+    # according to getLabelColor() [https://github.com/Project-MONAI/MONAILabel/blob/6cc72c542c9bc6c5181af89550e7e397537d74e3/plugins/slicer/MONAILabel/MONAILabel.py#L1485] # noqa
+    'lung': [128/255, 174/255, 128/255],  # green
+    'heart': [206/255, 110/255, 84/255],  # red
+}
+
 
 class TestWriter(unittest.TestCase):
     @parameterized.expand([WRITER_DATA])
@@ -28,6 +35,33 @@ class TestWriter(unittest.TestCase):
 
         file_ext = "".join(pathlib.Path(input_data["image_path"]).suffixes)
         self.assertIn(file_ext.lower(), [".nii", ".nii.gz"])
+
+    @parameterized.expand([WRITER_DATA])
+    def test_seg_nrrd(self, args, input_data):
+        args.update({"nibabel": False})
+        input_data["image_path"] = "fakepath.seg.nrrd"
+        input_data["labels"] = ["heart", "lung"]
+        input_data["color_map"] = COLOR_MAP
+
+        output_file, data = Writer(**args)(input_data)
+        self.assertEqual(os.path.exists(output_file), True)
+        arr_full, header = nrrd.read(output_file)
+
+        # DEBUG
+        print(header)
+
+        space_directions_expected = np.array([[np.nan, np.nan, np.nan],
+                                              [-1.,  0.,  0.],
+                                              [ 0., -1.,  0.],
+                                              [ 0.,  0.,  1.]])
+        self.assertTrue(np.array_equal(header['space directions'], space_directions_expected, equal_nan=True))
+
+        self.assertEqual(header['kinds'], ['list', 'domain', 'domain', 'domain'])
+        self.assertEqual(header['Segment1_ID'], 'lung')
+        self.assertEqual(header['Segment1_Color'], " ".join(map(str, COLOR_MAP['lung'])))
+
+        file_ext = "".join(pathlib.Path(input_data["image_path"]).suffixes)
+        self.assertIn(file_ext.lower(), [".seg.nrrd"])
 
     @parameterized.expand([WRITER_DATA])
     def test_itk(self, args, input_data):
