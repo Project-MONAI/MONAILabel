f5720f33f4f8371ba3e8c260a01b249c449ceb7a
Andres Diaz-Pinto <diazandr3s@gmail.com>
2022-05-14 12:14:22 +0100

    Prepare MONAI Label for new monai - DeepEdit transforms
    Signed-off-by: Andres Diaz-Pinto <diazandr3s@gmail.com>

--- sample-apps/radiology/lib/infers/segmentation_spleen.py
+++ sample-apps/radiology/lib/infers/segmentation_spleen.py
@@ -13,8 +13,8 @@ from typing import Callable, Sequence
 from monai.inferers import Inferer, SlidingWindowInferer
 from monai.transforms import (
     Activationsd,
-    AddChanneld,
     AsDiscreted,
+    EnsureChannelFirstd,
     EnsureTyped,
     LoadImaged,
     ScaleIntensityRanged,
@@ -54,7 +54,7 @@ class SegmentationSpleen(InferTask):
     def pre_transforms(self, data=None) -> Sequence[Callable]:
         return [
             LoadImaged(keys="image"),
-            AddChanneld(keys="image"),
+            EnsureChannelFirstd(keys="image"),
             Spacingd(keys="image", pixdim=[1.0, 1.0, 1.0]),
             ScaleIntensityRanged(keys="image", a_min=-57, a_max=164, b_min=0.0, b_max=1.0, clip=True),
             EnsureTyped(keys="image"),
