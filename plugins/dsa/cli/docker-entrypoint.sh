83338a8490a07bb118c9e444e5306cdebc98d5cb
David Manthey <david.manthey@kitware.com>
2022-05-30 16:34:00 -0400

    Remove some unused code in the dsa plugin docker. (#815)
    Update a value in the pathology app readme.

--- plugins/dsa/cli/docker-entrypoint.sh
+++ plugins/dsa/cli/docker-entrypoint.sh
@@ -1,14 +1,5 @@
 #!/usr/bin/env bash
 
-if type nvidia-smi >/dev/null 2>/dev/null;
-then
-    source /venv-gpu/bin/activate
-    echo "NOTE: GPU available" >&2
-fi
-
-# Try to start a local version memcached, but fail gracefully if we can't.
-memcached -u root -d -m 1024 || true
-
 # Calling the slicer_cli_web.cli_list_entrypoint always works, but we can skip
 # an extra exec if we find the path directly
 POSSIBLE_PATH="$1/$1.py"
