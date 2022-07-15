fd4bfc237d221e810f25da6e63741a22d515744b
Wenqi Li <831580+wyli@users.noreply.github.com>
2022-07-15 20:09:22 +0100

    update `monailabel/scribbles/infer.py` (#876)
    * adds to meta
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * update
    
    Signed-off-by: Wenqi Li <wenqil@nvidia.com>
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- monailabel/scribbles/infer.py
+++ monailabel/scribbles/infer.py
@@ -9,7 +9,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from monai.transforms import Compose, EnsureChannelFirstd, FromMetaTensord, LoadImaged, ScaleIntensityRanged, Spacingd
+from monai.transforms import (
+    Compose,
+    EnsureChannelFirstd,
+    FromMetaTensord,
+    LoadImaged,
+    ScaleIntensityRanged,
+    Spacingd,
+    ToMetaTensord,
+)
 
 from monailabel.interfaces.tasks.infer import InferTask, InferType
 from monailabel.scribbles.transforms import (
@@ -70,6 +78,7 @@ class ScribblesLikelihoodInferTask(InferTask):
                 scribbles_bg_label=self.scribbles_bg_label,
                 scribbles_fg_label=self.scribbles_fg_label,
             ),
+            ToMetaTensord(keys=["image", "label"]),
             Spacingd(keys=["image", "label"], pixdim=self.pix_dim, mode=["bilinear", "nearest"]),
             ScaleIntensityRanged(
                 keys="image",
@@ -103,6 +112,7 @@ class ScribblesLikelihoodInferTask(InferTask):
                 lamda=self.lamda,
                 sigma=self.sigma,
             ),
+            FromMetaTensord(keys=["image"]),
             Restored(keys="pred", ref_image="image"),
             BoundingBoxd(keys="pred", result="result", bbox="bbox"),
         ]
