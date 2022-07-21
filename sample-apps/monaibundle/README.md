01cea9fb873ab2a04fdd51643c68ec7c9a0aff5f
Sachidanand Alle <sachidanand.alle@gmail.com>
2022-07-20 20:29:57 -0700

    Support multi-gpu training + Fix docs
    Signed-off-by: Sachidanand Alle <sachidanand.alle@gmail.com>

--- sample-apps/monaibundle/README.md
+++ sample-apps/monaibundle/README.md
@@ -1,20 +1,58 @@
 # Integration with MONAI Bundle
+The App helps to pull any MONAI Bundle from [MONAI ZOO](https://github.com/Project-MONAI/model-zoo/tree/dev/models).
+However the following constraints has to be met for any monai bundle to directly import and use in MONAI Label.
+ - Has to meet [MONAI Bundle Specification](https://docs.monai.io/en/latest/mb_specification.html).
+ - For Inference, the bundle has defined **inference.json** or **inference.yaml** and defines [these keys](./lib/infers/bundle.py)
+ - For Training, the bundle has defined **train.json** or **train.yaml** and defines [these keys](./lib/trainers/bundle.py)
+ - For Multi-GPU Training, the bundle has defined **multi_gpu_train.json** or **multi_gpu_train.yaml**
+
+> By default models are picked from https://github.com/Project-MONAI/model-zoo/blob/dev/models/model_info.json
+
+### Structure of the App
+
+- **[lib/infers](./lib/infers)** is to define and activate inference task over monai-bundle.
+- **[lib/trainers](./lib/trainers)** is to define and activate training task over monai-bundle for single/multi gpu.
+- **[lib/activelearning](./lib/activelearning)** is the module to define the image selection techniques.
+- **[main.py](./main.py)** is the script to extend [MONAILabelApp](../../monailabel/interfaces/app.py) class
+
+> Modify Constants defined in [Infer](./lib/infers/bundle.py) and [Train](./lib/trainers/bundle.py) to customize and adopt if the basic standard/schema is not met for your bundle.
 
 ### Overview
-By default models are picked from https://github.com/Project-MONAI/model-zoo/blob/dev/models/model_info.json
 
-```commandline
-monailabel start_server -a sample-apps/monaibundle/ \
-  -s ~/Datasets/Radiology/ \
-  -c models spleen_ct_segmentation_v0.1.0
 
-monailabel start_server -a sample-apps/monaibundle/ \
-  -s ~/Datasets/Radiology/ \
-  -c models all
+### How To Use?
+```bash
+# skip this if you have already downloaded the app or using github repository (dev mode)
+monailabel apps --download --name monaibundle --output workspace
 
-# you can pass remove zoo info
-monailabel start_server -a sample-apps/monaibundle/ \
-  -s ~/Datasets/Radiology/ \
-  -c models all \
-  -c zoo_info https://raw.githubusercontent.com/Project-MONAI/model-zoo/54-auto-update-model-info/models/model_info.json
+# List all available models from zoo
+monailabel start_server --app workspace/monaibundle --studies workspace/images
+
+# Pick spleen_ct_segmentation_v0.1.0 model
+monailabel start_server --app workspace/monaibundle --studies workspace/images --conf models spleen_ct_segmentation_v0.1.0
+
+# Pick spleen_ct_segmentation_v0.1.0 model and preload
+monailabel start_server --app workspace/monaibundle --studies workspace/images --conf models spleen_ct_segmentation_v0.1.0 --conf preload true
+
+# Pick DeepEdit And Segmentation model (multiple models)
+monailabel start_server --app workspace/monaibundle --studies workspace/images --conf models "spleen_ct_segmentation_v0.1.0,spleen_deepedit_annotation_v0.1.0"
+
+# Pick All
+monailabel start_server --app workspace/monaibundle --studies workspace/images --conf models all
+
+# Pick All (Skip Training Tasks or Infer only mode)
+monailabel start_server --app workspace/radiology --studies workspace/images --conf models all --conf skip_trainers true
 ```
+
+
+
+#### Additional Configs
+Pass them as **--conf _name_ _value_** while starting MONAILabelServer
+
+| Name          | Values          | Description                                                                                 |
+|---------------|-----------------|---------------------------------------------------------------------------------------------|
+| zoo_info      | string          | _Default value:_ https://github.com/Project-MONAI/model-zoo/blob/dev/models/model_info.json |
+| zoo_source    | string          | _Default value:_ github                                                                     |
+| zoo_repo      | string          | _Default value:_ Project-MONAI/model-zoo/hosting_storage_v1                                 |
+| preload       | true, **false** | Preload model into GPU                                                                      |
+| skip_trainers | true, **false** | Skip adding training tasks (Run in Infer mode only)                                         |
--- sample-apps/monaibundle/lib/trainers/bundle.py
+++ sample-apps/monaibundle/lib/trainers/bundle.py
@@ -11,6 +11,7 @@
 import json
 import logging
 import os
+import subprocess
 
 import monai.bundle
 import torch
@@ -26,11 +27,13 @@ logger = logging.getLogger(__name__)
 
 class Const:
     CONFIGS = ("train.json", "train.yaml")
+    MULTI_GPU_CONFIGS = ("multi_gpu_train.json", "multi_gpu_train.yaml")
     METADATA_JSON = "metadata.json"
     MODEL_PYTORCH = "model.pt"
 
     KEY_DEVICE = "device"
     KEY_BUNDLE_ROOT = "bundle_root"
+    KEY_NETWORK = "network"
     KEY_NETWORK_DEF = "network_def"
     KEY_TRAIN_TRAINER_MAX_EPOCHS = "train#trainer#max_epochs"
     KEY_TRAIN_DATASET_DATA = "train#dataset#data"
@@ -95,15 +98,20 @@ class BundleTrainTask(TrainTask):
     def _device(self, str):
         return torch.device(str if torch.cuda.is_available() else "cpu")
 
-    def _load_checkpoint(self, output_dir, pretrained, multi_gpu, device, train_handlers):
+    def _load_checkpoint(self, output_dir, pretrained, train_handlers):
         load_path = os.path.join(output_dir, Const.MODEL_PYTORCH) if pretrained else None
         if os.path.exists(load_path):
             logger.info(f"Add Checkpoint Loader for Path: {load_path}")
 
-            map_location = {"cuda:0": f"cuda:{device.index}" if device.index else "cuda"} if multi_gpu else None
-            network = self.bundle_config.get_parsed_content(Const.KEY_NETWORK_DEF, instantiate=True)
-            load_dict = {"model": network}
-            train_handlers.append(CheckpointLoader(load_path, load_dict, map_location=map_location, strict=False))
+            load_dict = {"model": f"$@{Const.KEY_NETWORK}"}
+            if not [t for t in train_handlers if t.get("_target_") == CheckpointLoader.__name__]:
+                loader = {
+                    "_target_": CheckpointLoader.__name__,
+                    "load_path": load_path,
+                    "load_dict": load_dict,
+                    "strict": False,
+                }
+                train_handlers.insert(0, loader)
 
     def __call__(self, request, datastore: Datastore):
         ds = datastore.datalist()
@@ -112,12 +120,18 @@ class BundleTrainTask(TrainTask):
         max_epochs = request.get("max_epochs", 50)
         pretrained = request.get("pretrained", True)
         multi_gpu = request.get("multi_gpu", False)
+        multi_gpu = multi_gpu if torch.cuda.device_count() > 1 else False
+
+        gpus = request.get("gpus", "all")
+        gpus = list(range(torch.cuda.device_count())) if gpus == "all" else [int(g) for g in gpus.split(",")]
+        logger.info(f"Using Multi GPU: {multi_gpu}; GPUS: {gpus}")
+        logger.info(f"CUDA_VISIBLE_DEVICES: {os.environ.get('CUDA_VISIBLE_DEVICES')}")
 
         device = self._device(request.get("device", "cuda"))
         logger.info(f"Using device: {device}")
 
         train_handlers = self.bundle_config.get(Const.KEY_TRAIN_HANDLERS, [])
-        self._load_checkpoint(os.path.join(self.bundle_path, "models"), pretrained, multi_gpu, device, train_handlers)
+        self._load_checkpoint(os.path.join(self.bundle_path, "models"), pretrained, train_handlers)
 
         overrides = {
             Const.KEY_BUNDLE_ROOT: self.bundle_path,
@@ -131,11 +145,60 @@ class BundleTrainTask(TrainTask):
         if val_ds is not None:
             overrides[Const.KEY_VALIDATE_DATASET_DATA] = val_ds
 
-        monai.bundle.run(
-            "training",
-            meta_file=self.bundle_metadata_path,
-            config_file=self.bundle_config_path,
-            **overrides,
-        )
+        if multi_gpu:
+            config_paths = [
+                c for c in Const.MULTI_GPU_CONFIGS if os.path.exists(os.path.join(self.bundle_path, "configs", c))
+            ]
+            if not config_paths:
+                logger.warning(f"Ignore Multi-GPU Training; No multi-gpu train config {Const.MULTI_GPU_CONFIGS} exists")
+                return
+
+            train_path = os.path.join(self.bundle_path, "configs", "monailabel_train.json")
+            multi_gpu_train_path = os.path.join(self.bundle_path, "configs", config_paths[0])
+            logging_file = os.path.join(self.bundle_path, "configs", "logging.conf")
+            for k, v in overrides.items():
+                if k != Const.KEY_DEVICE:
+                    self.bundle_config.set(v, k)
+            ConfigParser.export_config_file(self.bundle_config.config, train_path, indent=2)
+
+            env = os.environ.copy()
+            env["CUDA_VISIBLE_DEVICES"] = ",".join([str(g) for g in gpus])
+            logger.info(f"Using CUDA_VISIBLE_DEVICES: {env['CUDA_VISIBLE_DEVICES']}")
+            cmd = [
+                "torchrun",
+                "--standalone",
+                "--nnodes=1",
+                f"--nproc_per_node={len(gpus)}",
+                "-m",
+                "monai.bundle",
+                "run",
+                "training",
+                "--meta_file",
+                self.bundle_metadata_path,
+                "--config_file",
+                f"['{train_path}','{multi_gpu_train_path}']",
+                "--logging_file",
+                logging_file,
+            ]
+            self.run_command(cmd, env)
+        else:
+            monai.bundle.run(
+                "training",
+                meta_file=self.bundle_metadata_path,
+                config_file=self.bundle_config_path,
+                **overrides,
+            )
+
         logger.info("Training Finished....")
         return {}
+
+    def run_command(self, cmd, env):
+        process = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True, env=env)
+        while process.poll() is None:
+            line = process.stdout.readline()
+            line = line.rstrip()
+            if line:
+                print(line, flush=True)
+
+        logger.info(f"Return code: {process.returncode}")
+        process.stdout.close()
