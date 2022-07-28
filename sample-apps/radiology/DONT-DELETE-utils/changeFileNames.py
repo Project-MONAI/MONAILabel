822b4789f26171017b9d2de8dedc4937e337c937
Andres <diazandr3s@gmail.com>
2022-07-28 01:28:37 +0100

    Create dictionary with all label names - trigger training using Swin UNETR
    Signed-off-by: Andres <diazandr3s@gmail.com>

---
+++ sample-apps/radiology/DONT-DELETE-utils/changeFileNames.py
@@ -0,0 +1,28 @@
+# Copyright (c) MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import glob
+import os
+import shutil
+import time
+
+data_dir = "/home/andres/Documents/workspace/Datasets/radiology/BRATS-2021/allSegments/monailabel/labels/final/"
+output_folder = (
+    "/home/andres/Documents/workspace/Datasets/radiology/BRATS-2021/allSegments/monailabel/labels/fileChanged/"
+)
+
+all_files = glob.glob(os.path.join(data_dir, "*.nii.gz"))
+
+
+for idx, img_path in enumerate(all_files):
+    fname = img_path.split("/")[-1]
+    print(f"Processing image: {idx}/{len(all_files)}")
+    time.sleep(1)
+    shutil.copy(img_path, output_folder + fname[:15] + ".nii.gz")
