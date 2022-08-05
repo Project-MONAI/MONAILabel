8ae7f553c7cb7bfc7fbb0b7d34b989d957f4cc26
Sachidanand Alle <sachidanand.alle@gmail.com>
2022-08-05 11:03:42 -0700

    Fix load path for training tasks
    Signed-off-by: Sachidanand Alle <sachidanand.alle@gmail.com>

--- sample-apps/radiology/lib/configs/segmentation_spleen.py
+++ sample-apps/radiology/lib/configs/segmentation_spleen.py
@@ -92,11 +92,13 @@ class SegmentationSpleen(TaskConfig):
 
     def trainer(self) -> Optional[TrainTask]:
         output_dir = os.path.join(self.model_dir, self.name)
+        load_path = self.path[0] if os.path.exists(self.path[0]) else self.path[1]
+
         task: TrainTask = lib.trainers.SegmentationSpleen(
             model_dir=output_dir,
             network=self.network,
             description="Train Spleen Segmentation Model",
-            load_path=self.path[0],
+            load_path=load_path,
             publish_path=self.path[1],
             labels=self.labels,
         )
