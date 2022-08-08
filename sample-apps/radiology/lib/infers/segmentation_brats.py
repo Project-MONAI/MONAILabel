e9d256669b6f95c4200e08a49bac28a2afcf0858
Andres <diazandr3s@gmail.com>
2022-08-08 14:59:56 +0100

    Update largest connected components
    Signed-off-by: Andres <diazandr3s@gmail.com>

--- sample-apps/radiology/lib/infers/segmentation_brats.py
+++ sample-apps/radiology/lib/infers/segmentation_brats.py
@@ -16,6 +16,7 @@ from monai.transforms import (
     AsDiscreted,
     EnsureChannelFirstd,
     EnsureTyped,
+    KeepLargestConnectedComponentd,
     LoadImaged,
     NormalizeIntensityd,
     ToNumpyd,
@@ -69,10 +70,15 @@ class SegmentationBrats(InferTask):
         return []
 
     def post_transforms(self, data=None) -> Sequence[Callable]:
-        return [
+        largest_cc = False if not data else data.get("largest_cc", False)
+        applied_labels = list(self.labels.values()) if isinstance(self.labels, dict) else self.labels
+        t = [
             EnsureTyped(keys="pred", device=data.get("device") if data else None),
-            Activationsd(keys="pred", softmax=len(self.labels) > 1, sigmoid=len(self.labels) == 1),
-            AsDiscreted(keys="pred", argmax=len(self.labels) > 1, threshold=0.5 if len(self.labels) == 1 else None),
+            Activationsd(keys="pred", softmax=True),
+            AsDiscreted(keys="pred", argmax=True),
             ToNumpyd(keys="pred"),
-            Restored(keys="pred", ref_image="image"),
         ]
+        if largest_cc:
+            t.append(KeepLargestConnectedComponentd(keys="pred", applied_labels=applied_labels))
+        t.append(Restored(keys="pred", ref_image="image"))
+        return t
