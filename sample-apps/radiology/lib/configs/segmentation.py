161db4b5c69ef8bd285fbd566df4bd7bd744b374
Andres <diazandr3s@gmail.com>
2022-08-09 00:38:23 +0100

    Change flag for pretrained model
    Signed-off-by: Andres <diazandr3s@gmail.com>

--- sample-apps/radiology/lib/configs/segmentation.py
+++ sample-apps/radiology/lib/configs/segmentation.py
@@ -56,7 +56,7 @@ class Segmentation(TaskConfig):
         ]
 
         # Download PreTrained Model
-        if strtobool(self.conf.get("use_pretrained_model", "false")):
+        if strtobool(self.conf.get("use_pretrained_model", "true")):
             url = f"{self.conf.get('pretrained_path', self.PRE_TRAINED_PATH)}/segmentation_unet_multilabel.pt"
             download_file(url, self.path[0])
 
