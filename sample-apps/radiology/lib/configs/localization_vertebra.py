5b641e03223b44a8d3a1d89f179df282faf335e0
Andres <diazandr3s@gmail.com>
2022-08-23 14:12:46 +0100

    Update localization vertebra script
    Signed-off-by: Andres <diazandr3s@gmail.com>

--- sample-apps/radiology/lib/configs/localization_vertebra.py
+++ sample-apps/radiology/lib/configs/localization_vertebra.py
@@ -68,7 +68,7 @@ class LocalizationVertebra(TaskConfig):
             url = f"{self.conf.get('pretrained_path', self.PRE_TRAINED_PATH)}/localization_vertebra_unet.pt"
             download_file(url, self.path[0])
 
-        self.target_spacing = (1.5, 1.5, 1.5)  # target space for image
+        self.target_spacing = (1.3, 1.3, 1.3)  # target space for image
         # Setting ROI size - This is for the image padding
         self.roi_size = (96, 96, 96)
 
@@ -77,8 +77,8 @@ class LocalizationVertebra(TaskConfig):
             spatial_dims=3,
             in_channels=1,
             out_channels=len(self.labels) + 1,  # labels plus background,
-            channels=(16, 32, 64, 128, 256),
-            strides=(2, 2, 2, 2),
+            channels=(16, 32, 64, 128),
+            strides=(2, 2, 2),
             num_res_units=2,
             dropout=0.2,
         )
@@ -91,7 +91,7 @@ class LocalizationVertebra(TaskConfig):
             target_spacing=self.target_spacing,
             labels=self.labels,
             preload=strtobool(self.conf.get("preload", "false")),
-            config={"largest_cc": True, "result_mask": False},
+            config={"largest_cc": True, "result_mask": True},
         )
         return task
 
