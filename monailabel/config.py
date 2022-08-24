b8209726e2c14a06913a362e23aa7803df3cdb1a
Janis Vahldiek <janis@vahldiek.info>
2022-08-24 16:48:08 +0200

    Update config.py

--- monailabel/config.py
+++ monailabel/config.py
@@ -47,6 +47,7 @@ class Settings(BaseSettings):
     MONAI_LABEL_WADO_PREFIX: str = ""
     MONAI_LABEL_STOW_PREFIX: str = ""
     MONAI_LABEL_DICOMWEB_FETCH_BY_FRAME: bool = False
+    MONAI_LABEL_DICOMWEB_SEARCH_FILTER: Dict[str, str] = {"Modality": "CT"}
 
     MONAI_LABEL_DATASTORE_AUTO_RELOAD: bool = True
     MONAI_LABEL_DATASTORE_FILE_EXT: List[str] = [
