Dimension mismatch fixed to use MONAI Variance Metric function
Signed-off-by: Vishwesh Nath <vnath@nvidia.com>

monailabel/tasks/scoring/epistemic_v2.py
@@ -91,8 +91,8 @@ class EpistemicScoring(ScoringMethod):
             variance = np.sum(vari, axis=0)
         else:
             variance_metric = VarianceMetric(threshold=0.0005, spatial_map=True, scalar_reduction="sum")
-            variance = variance_metric(vol_input).cpu().detach().numpy()
-
+            variance = variance_metric(vol_input)
+            
         if self.dimension == 3:
             variance = np.expand_dims(variance, axis=0)
             variance = np.expand_dims(variance, axis=0)
@@ -181,14 +181,17 @@ class EpistemicScoring(ScoringMethod):
 
         accum = torch.stack(accum_unl_outputs)
         accum = torch.squeeze(accum)
-        if self.dimension == 3:
-            accum = accum[:, 1:, :, :, :] if len(accum.shape) > 4 else accum
-        else:
-            accum = accum[:, 1:, :, :] if len(accum.shape) > 3 else accum
-
+        
+        # Accum Expected shape for 2D images is (N, C, H, W) for 3D (N, C, H, W, D)
+        # To handle cases where only a single class of segmentation is present, an extra dimension is added 
+        if self.dimension==2 and len(accum.shape) == 3: 
+            accum = torch.unsqueeze(accum, dim=1)
+        elif self.dimension==3 and len(accum.shape) == 4:
+            accum = torch.unsqueeze(accum, dim=1)
+        
         entropy = self.variance_volume(accum) if self.use_variance else self.entropy_volume(accum)
         entropy = float(np.nanmean(entropy))
-
+        
         latency = time.time() - start
         logger.info(
             "EPISTEMIC:: {} => iters: {}; entropy: {}; latency: {};".format(