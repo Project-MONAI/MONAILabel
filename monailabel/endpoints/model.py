ac5f3cee176371597b19a3db1784c79db672eb27
Sachidanand Alle <sachidanand.alle@gmail.com>
2022-10-14 13:03:01 -0700

    Fix group tag
    Signed-off-by: Sachidanand Alle <sachidanand.alle@gmail.com>

--- monailabel/endpoints/model.py
+++ monailabel/endpoints/model.py
@@ -24,7 +24,7 @@ logger = logging.getLogger(__name__)
 
 router = APIRouter(
     prefix="/model",
-    tags=["AppService"],
+    tags=["Model"],
     responses={404: {"description": "Not found"}},
 )
 
