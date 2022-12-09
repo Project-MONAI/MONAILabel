9e044a996bf403b651aa6a9ba09b4b1419e7e3d6
Sachidanand Alle <sachidanand.alle@gmail.com>
2022-12-09 05:18:35 +0000

    support multi thread/multi gpu batch infer
    Signed-off-by: Sachidanand Alle <sachidanand.alle@gmail.com>

--- monailabel/interfaces/tasks/batch_infer.py
+++ monailabel/interfaces/tasks/batch_infer.py
@@ -11,8 +11,13 @@
 
 import copy
 import logging
+import multiprocessing
+import time
+from concurrent.futures import ThreadPoolExecutor
 from enum import Enum
 from typing import Callable
+
+import torch
 
 from monailabel.interfaces.datastore import Datastore, DefaultLabelTag
 from monailabel.utils.others.generic import remove_file
@@ -48,19 +53,80 @@ class BatchInferTask:
         return images
 
     def __call__(self, request, datastore: Datastore, infer: Callable):
-        image_ids = self.get_images(request, datastore)
-        logger.info(f"Total number of images for batch inference: {len(image_ids)}")
+        image_ids = sorted(self.get_images(request, datastore))
+        max_batch_size = request.get("max_batch_size", 0)
+        logger.info(f"Total number of images for batch inference: {len(image_ids)}; Max Batch Size: {max_batch_size}")
 
-        result = {}
-        for image_id in image_ids:
+        start = time.time()
+        if max_batch_size > 0:
+            image_ids = image_ids[:max_batch_size]
+
+        multi_gpu = request.get("multi_gpu", True)
+        multi_gpus = request.get("gpus", "all")
+        gpus = (
+            list(range(torch.cuda.device_count())) if not multi_gpus or multi_gpus == "all" else multi_gpus.split(",")
+        )
+        device_ids = [f"cuda:{id}" for id in gpus] if multi_gpu else [request.get("device", "cuda")]
+
+        result: dict = {}
+        infer_tasks = []
+        for idx, image_id in enumerate(image_ids):
             req = copy.deepcopy(request)
+            req["_id"] = idx
+            req["_image_id"] = image_id
+
             req["image"] = image_id
             req["save_label"] = True
             req["label_tag"] = DefaultLabelTag.ORIGINAL
+            req["logging"] = request.get("logging", "INFO")
+            req["device"] = device_ids[idx % len(device_ids)]
 
-            logger.info(f"Running inference for image id {image_id}")
-            r = infer(req, datastore)
-            if r.get("file"):
-                remove_file(r.get("file"))
-            result["image_id"] = r
+            infer_tasks.append(req)
+            result[image_id] = None
+
+        total = len(infer_tasks)
+        max_workers = request.get("max_workers", 0)
+        max_workers = max_workers if max_workers else max(1, multiprocessing.cpu_count() // 4)
+        max_workers = min(max_workers, multiprocessing.cpu_count())
+
+        if len(infer_tasks) > 1 and (max_workers == 0 or max_workers > 1):
+            logger.info(f"MultiGpu: {multi_gpu}; Using Device(s): {device_ids}; Max Workers: {max_workers}")
+            futures = {}
+            with ThreadPoolExecutor(max_workers if max_workers else None, "WSI Infer") as executor:
+                for t in infer_tasks:
+                    futures[t["_id"]] = t, executor.submit(run_infer_task, t, datastore, infer)
+
+                for tid, (t, future) in futures.items():
+                    image_id = t["_image_id"]
+                    try:
+                        res = future.result()
+                        result[image_id] = res
+                    except Exception:
+                        logger.warning(f"Failed to finish Infer Task: {tid} => {image_id}", exc_info=True)
+                        result[image_id] = {}
+
+                    finished = len([a for a in result.values() if a is not None])
+                    logger.info(f"{tid} => {image_id} => {t['device']} => {finished} / {total}")
+        else:
+            for t in infer_tasks:
+                tid = t["_id"]
+                res = run_infer_task(t, datastore, infer)
+
+                image_id = t["_image_id"]
+                result[image_id] = res
+                finished = tid + 1
+                logger.info(f"{tid} => {image_id} => {t['device']} => {finished} / {total}")
+
+        latency_total = time.time() - start
+        logger.info(f"Batch Infer Time Taken: {latency_total:.4f}")
         return result
+
+
+def run_infer_task(req, datastore, infer):
+    image_id = req.get("image")
+    logger.info(f"Running inference for image id {image_id}")
+    r = infer(req, datastore)
+    if r.get("file"):
+        remove_file(r.get("file"))
+        r.pop("file")
+    return r
