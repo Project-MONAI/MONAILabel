779da8bde4d11c3ff6d421a40a557965c51fa4be
tangy5 <yucheng.tang@vanderbilt.edu>
2022-12-11 23:47:12 -0800

    Add Unit Tests to improve coverage - 1
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>

--- tests/unit/transform/test_post.py
+++ tests/unit/transform/test_post.py
@@ -9,12 +9,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
 import unittest
-
+import tempfile
 import numpy as np
 from parameterized import parameterized
 
-from monailabel.transform.post import BoundingBoxd, ExtremePointsd, LargestCCd, Restored
+from monailabel.transform.post import BoundingBoxd, ExtremePointsd, LargestCCd, Restored, FindContoursd, DumpImagePrediction2Dd, MergeAllPreds, RenameKeyd
 
 CCD_DATA = [
     {"keys": ("pred",)},
@@ -45,6 +46,34 @@ RESTORED_DATA = [
     (1, 6, 6),
 ]
 
+FINDCONTOURSD_DATA = [
+    {"keys": "pred", "labels": "Other", "min_positive": 4, "min_poly_area": 1},
+    {
+        "pred": np.array([[0, 0, 0, 0, 0], [0, 1, 1, 1, 0], [0, 1, 0, 1, 0], [0, 1, 1, 1, 0], [0, 0, 0, 0, 0]]),
+    },
+    [[[1, 2], [2, 1], [3, 2], [2, 3]], [[1, 1], [1, 3], [3, 3], [3, 1]]],
+]
+
+DUMPIMAGEPREDICTION2DD_DATA = [
+    {
+        "image": np.random.rand(1, 3, 5, 5),
+        "pred": np.random.rand(1, 5, 5),
+    },
+]
+
+METGEAllPREDS_DATA = [
+    {"keys": ["pred", "pred_2"]},
+    {
+        "pred": np.array([[[0, 0, 0, 0], [0, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 0]]]),
+        "pred_2": np.array([[[1, 0, 0, 1], [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]]),
+    },
+    [[[1, 0, 0, 1], [1, 1, 1, 0], [0, 1, 1, 0], [0, 0, 0, 1]]],
+]
+
+RENAMEKEY_DATA = [
+    {"source_key": "pred", "target_key": "pred_2"},
+    {"pred": np.array([[[0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]])},
+]
 
 class TestLargestCCd(unittest.TestCase):
     @parameterized.expand([CCD_DATA])
@@ -52,13 +81,11 @@ class TestLargestCCd(unittest.TestCase):
         res = LargestCCd(**args)(input_data)
         np.testing.assert_equal(res["pred"], expected_output)
 
-
 class TestExtremePointsd(unittest.TestCase):
     @parameterized.expand([EXTREME_POINTS_DATA])
     def test_result(self, args, input_data, expected_data):
         res = ExtremePointsd(**args)(input_data)
         self.assertEqual(res["result"]["points"], expected_data)
-
 
 class TestBoundingBoxd(unittest.TestCase):
     @parameterized.expand([BB_DATA])
@@ -66,13 +93,39 @@ class TestBoundingBoxd(unittest.TestCase):
         res = BoundingBoxd(**args)(input_data)
         self.assertEqual(res["result"]["bbox"], expected_data)
 
-
 class TestRestored(unittest.TestCase):
     @parameterized.expand([RESTORED_DATA])
     def test_result(self, args, input_data, expected_shape):
         res = Restored(**args)(input_data)
         self.assertEqual(res["pred"].shape, expected_shape)
 
+class TestFindContoursd(unittest.TestCase):
+    @parameterized.expand([FINDCONTOURSD_DATA])
+    def test_result(self, args, input_data, expected_output):
+        res = FindContoursd(**args)(input_data)
+        self.assertEqual(res["result"]["annotation"]["elements"][0]["contours"], expected_output)
+
+class TestDumpImagePrediction2Dd(unittest.TestCase):
+    @parameterized.expand([DUMPIMAGEPREDICTION2DD_DATA])
+    def test_saved_content(self, input_data):
+        with tempfile.TemporaryDirectory() as tempdir:
+            image_path = os.path.join(tempdir, "testimage.png")
+            pred_path = os.path.join(tempdir, "testpred.png")
+            _ = DumpImagePrediction2Dd(image_path=image_path, pred_path=pred_path, pred_only=False)(input_data)
+            self.assertTrue(os.path.exists(image_path))
+            self.assertTrue(os.path.exists(pred_path))
+
+class TestMergeAllPreds(unittest.TestCase):
+    @parameterized.expand([METGEAllPREDS_DATA])
+    def test_merge_pred(self, args, input_data, expected_output):
+        res = MergeAllPreds(**args)(input_data)
+        self.assertEqual(res.tolist(), expected_output)
+
+class TestRenameKeyd(unittest.TestCase):
+    @parameterized.expand([RENAMEKEY_DATA])
+    def test_rename_key(self, args, input_data):
+        res = RenameKeyd(**args)(input_data)
+        self.assertEqual(list(res.keys())[0], args["target_key"])
 
 if __name__ == "__main__":
     unittest.main()
