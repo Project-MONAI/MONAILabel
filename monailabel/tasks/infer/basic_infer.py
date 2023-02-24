814e86aa7401cafeeabc78ed148fe7d007495858
tangy5 <yucheng.tang@vanderbilt.edu>
2023-02-23 16:16:33 -0800

    sanity checks
    Signed-off-by: tangy5 <yucheng.tang@vanderbilt.edu>

--- monailabel/tasks/infer/basic_infer.py
+++ monailabel/tasks/infer/basic_infer.py
@@ -268,7 +268,9 @@ class BasicInferTask(InferTask):
         req.update(request)
 
         # model options
-        self.path.append(os.path.join(os.path.dirname(self.path[0]), req.get("model_filename", "model.pt"))) if isinstance(self.path, list) else self.path
+        self.path.append(
+           os.path.join(os.path.dirname(self.path[0]), req.get("model_filename", "model.pt"))
+        ) if self.path and isinstance(self.path, list) else self.path
 
         # device
         device = req.get("device", "cuda")
